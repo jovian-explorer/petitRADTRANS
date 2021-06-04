Updated retrieval tutorial, corrected error bar sizes, fixed plotting bugs, fixed corner plot scaling.

petitRADTRANS/retrieval/models.py
@@ -14,14 +14,10 @@ from petitRADTRANS import nat_cst as nc
 from petitRADTRANS.retrieval import cloud_cond as fc
 import pdb
 
-try: 
-    from poor_mans_nonequ_chem import poor_mans_nonequ_chem as pm
-except ImportError:
-    print("Could not import poor_mans_nonequ_chemistry. Exiting.")
-    sys.exit(2)     
+    
 
 # Global constants to reduce calculations and initializations.
-p_global = np.logspace(-6,3,100*10)
+p_global = np.logspace(-6,3,1000)
 
 #######################################################
 # Define retrieval models
@@ -94,6 +90,10 @@ def emission_model_diseq(pRT_object,
         Computed emission spectrum [W/m2/micron]
     """
     
+
+    pglobal_check(pRT_object.press,
+                    parameters['pressure_simple'].value,
+                    parameters['pressure_scaling'].value)
     #for key, val in parameters.items():
     #    print(key,val.value)
     # Priors for these parameters are implemented here, as they depend on each other
@@ -201,10 +201,13 @@ def guillot_free_emission(pRT_object, \
     spectrum_model : np.array
         Computed emission spectrum [W/m2/micron]
     """
-    # Make the P-T profile
-    #pressures = pRT_object.press/1e6
     #for key, val in parameters.items():
     #    print(key,val.value)
+
+    pglobal_check(pRT_object.press,
+                  parameters['pressure_simple'].value,
+                  parameters['pressure_scaling'].value)
+
     temperatures = nc.guillot_global(p_global, \
                                 10**parameters['log_kappa_IR'].value, 
                                 parameters['gamma'].value, \
@@ -322,6 +325,11 @@ def guillot_eqchem_transmission(pRT_object, \
     spectrum_model : np.array
         Computed transmission spectrum R_pl**2/Rstar**2
     """
+    try: 
+        from poor_mans_nonequ_chem import poor_mans_nonequ_chem as pm
+    except ImportError:
+        print("Could not import poor_mans_nonequ_chemistry. Exiting.")
+        sys.exit(2) 
     # Make the P-T profile
     pressures = pRT_object.press/1e6
     temperatures = nc.guillot_global(pressures, \
@@ -418,6 +426,11 @@ def isothermal_eqchem_transmission(pRT_object, \
     spectrum_model : np.array
         Computed transmission spectrum R_pl**2/Rstar**2
     """
+    try: 
+        from poor_mans_nonequ_chem import poor_mans_nonequ_chem as pm
+    except ImportError:
+        print("Could not import poor_mans_nonequ_chemistry. Exiting.")
+        sys.exit(2)
     # Make the P-T profile
     pressures = pRT_object.press/1e6
     temperatures = parameters['Temp'].value * np.ones_like(pressures)
@@ -591,7 +604,11 @@ def PT_ret_model(T3, delta, alpha, tint, press, FeH, CO, conv = True):
     CO: C/O for the nabla_ad interpolation
     FeH: metallicity for the nabla_ad interpolation
     '''
-
+    try: 
+        from poor_mans_nonequ_chem import poor_mans_nonequ_chem as pm
+    except ImportError:
+        print("Could not import poor_mans_nonequ_chemistry. Exiting.")
+        sys.exit(2)
     # Go grom bar to cgs
     press_cgs = press*1e6
 
@@ -734,6 +751,7 @@ def PT_ret_model(T3, delta, alpha, tint, press, FeH, CO, conv = True):
     return tret#, press_tau(1.)/1e6, tfintp(p_bot_spline)
 
 def make_half_pressure_better(P_clouds, press):
+    # Obsolete, replaced with fixed_length_amr
     press_plus_index = np.zeros((press.shape[0],2))
     press_plus_index[:,0] = press
     press_plus_index[:,1] = range(len(press))
@@ -763,11 +781,24 @@ def make_half_pressure_better(P_clouds, press):
     return press_out[:,0],  press_out[:, 1].astype('int')
 
 def fixed_length_amr(P_clouds, press, scaling = 10, width = 3):
+    """
+    fixed_length_amr
+    This function takes in the cloud base pressures for each cloud,
+    and returns an array of pressures with a high resolution mesh 
+    in the region where the cloud is located.
+
+    parameters
+    ----------
+    P_clouds : numpy.ndarray
+        The cloud base pressures in bar
+    press : np.ndarray
+    """
     # P_clouds is array of pressures
     # press should be ~len scaling*100
     # guarantees total length will be press.shape[0] + P_clouds.shape[0]*width*(scaling - 1)
     # scaling is how many hi-res points per normal point. Must be int
     # width is the number of low res points to replace. Must be int.
+
     press_plus_index = np.zeros((press.shape[0],2))
     press_plus_index[:,0] = np.logspace(np.log10(np.min(press)),np.log10(np.max(press)),press.shape[0])
     press_plus_index[:,1] = range(len(press_plus_index[:,0]))
@@ -860,6 +891,40 @@ def fixed_length_amr(P_clouds, press, scaling = 10, width = 3):
 
 
 def get_abundances(pRT_object,pressures,temperatures,parameters,AMR = False):
+    """
+    get_abundances
+    This function takes in the C/O ratio, metallicity, and quench pressures and uses them
+    to compute the gas phase and equilibrium condensate abundances.
+    Clouds are currently hard coded into the function.
+
+    parameters:
+    -----------
+    pRT_object : petitRADTRANS.RadTrans
+        The pRT object used to compute the spectrum.
+    pressures : numpy.ndarray
+        A log spaced pressure array. If AMR is on it should be the full high resolution grid.
+    temperatures : numpy.ndarray
+        A temperature array with the same shape as pressures
+    parameters : dict
+        A dictionary of model parameters, in particular it must contain the names C/O, Fe/H and
+        log_pquench. Additionally the cloud parameters log_X_cb_Fe(c) and MgSiO3(c) must be present.
+    AMR : bool
+        Turn the adaptive mesh grid on or off. See fixed_length_amr for implementation.
+
+    returns:
+    --------
+    abundances : dict
+        Mass fraction abundances of all atmospheric species
+    MMW : numpy.ndarray
+        Array of the mean molecular weights in each pressure bin
+    small_index : numpy.ndarray
+        The indices of the high resolution grid to use to define the adaptive grid.
+    """
+    try: 
+        from poor_mans_nonequ_chem import poor_mans_nonequ_chem as pm
+    except ImportError:
+        print("Could not import poor_mans_nonequ_chemistry. Exiting.")
+        sys.exit(2)
     # Make the abundance profile
     COs = parameters['C/O'].value * np.ones_like(pressures)
     FeHs = parameters['Fe/H'].value * np.ones_like(pressures)
@@ -895,7 +960,13 @@ def get_abundances(pRT_object,pressures,temperatures,parameters,AMR = False):
     for key, val in Pbases.items():
         p_clouds.append(val)
     p_clouds = np.array(p_clouds)
-    press_use, small_index = fixed_length_amr(p_clouds, pressures)
+
+    if AMR:
+        press_use, small_index = fixed_length_amr(p_clouds, pressures)
+    else : 
+        #TODO: Test
+        press_use = pressures
+        small_index = np.indices(press_use)
 
     for cloud in cp.copy(pRT_object.cloud_species):
         cname = cloud.split('_')[0]
@@ -924,6 +995,28 @@ def get_abundances(pRT_object,pressures,temperatures,parameters,AMR = False):
     if 'FeH' in abundances.keys():
         abundances['FeH'] = abundances['FeH']/2.
     return abundances,MMW,small_index
+
+def pglobal_check(press,shape,scaling):
+    """
+    pglobal_check
+    Check to ensure that the global pressure array has the correct length.
+    Updates p_global.
+
+    parameters
+    ----------
+    press : numpy.ndarray
+        Pressure array from a pRT_object. Used to set the min and max values of p_global
+    shape : int
+        the shape of the pressure array if no AMR is used
+    scaling : 
+        The factor by which the pressure array resolution should be scaled.
+    """
+    global p_global
+    if p_global.shape[0] != int(scaling*shape):  
+        p_global = np.logspace(np.log10(press[0]),
+                                np.log10(press[1]),
+                                int(scaling*shape))
+    return
 
 def set_resolution(lines,abundances,resolution):
     # Set correct key names in abundances for pRT, with set resolution

petitRADTRANS/retrieval/retrieval.py
@@ -1,6 +1,4 @@
-###########################################
 # Input / output, general run definitions
-###########################################
 import sys, os
 # To not have numpy start parallelizing on its own
 os.environ["OMP_NUM_THREADS"] = "1"
@@ -8,16 +6,16 @@ os.environ["OMP_NUM_THREADS"] = "1"
 # Read external packages
 import numpy as np
 import copy as cp
-
 import pymultinest
 import json
 import argparse as ap
 from scipy.stats import binned_statistic
+
+# Plotting
 import matplotlib.pyplot as plt
 from matplotlib.ticker import AutoMinorLocator, MultipleLocator, AutoMinorLocator, LogLocator, NullFormatter
 
 # Read own packages
-import petitRADTRANS # Just need the filename actually
 from petitRADTRANS import Radtrans
 from petitRADTRANS import nat_cst as nc
 from .parameter_class import Parameter
@@ -40,7 +38,7 @@ class Retrieval:
                  pRT_plot_style = True,
                  plot_multiple_retrieval_names = None):
         """
-        Retrieve Class
+        Retrieval Class
         This class implements the retrieval method using petitRADTRANS and pymultinest.
         A RetrievalConfig object is passed to this class to describe the retrieval data, parameters
         and priors. The run() method then uses pymultinest to sample the parameter space, producing
@@ -181,82 +179,39 @@ class Retrieval:
                 print(fmts % (p, med, sigma))
         return
 
-    def getSamples(self, output_dir = None):
+    def run_ultranest(self):
         """
-        getSamples
-        This function looks in the given output directory and finds the post_equal_weights
-        file associated with the current retrieval name.
-
-        parameters
-        ----------
-        output_dir : str
-            Parent directory of the out_PMN/*post_equal_weights.dat file
-        
-        returns
-        -------
-        sample_dict : dict
-            A dictionary with keys being the name of the retrieval, and values are a numpy
-            ndarray containing the samples in the post_equal_weights file
-        parameter_dict : dict
-            A dictionary with keys being the name of the retrieval, and values are a list of names
-            of the parameters used in the retrieval. The first name corresponds to the first column
-            of the samples, and so on.
+        run
+        Run mode for the class. Uses ultranest to sample parameter space
+        and produce standard outputs.
         """
-        sample_dict = {}
-        parameter_dict = {}
-        if output_dir is None:
-            output_dir = self.output_dir
-        for name in self.corner_files:
-            samples = np.genfromtxt(output_dir +'out_PMN/'+ \
-                                    name+ \
-                                    '_post_equal_weights.dat')
+        print("Warning, ultranest mode is still in development. Proceed with caution")
+        try:
+             import ultranest as un
+        except ImportError:
+            print("Could not import ultranest. Exiting.")
+            sys.exit(1)
+        # Todo: autodetect PMN vs UN outputs
+        prefix = self.output_dir + 'out_PMN/'+self.retrieval_name+'_'
+        if self.run_mode == 'retrieval':
+            print("Starting retrieval: " + self.retrieval_name+'\n')
+            # How many free parameters?
+            n_params = 0
+            free_parameter_names = []
+            for pp in self.parameters:
+                if self.parameters[pp].is_free_parameter:
+                    free_parameter_names.append(self.parameters[pp].name)
+                    n_params += 1
+            json.dump(free_parameter_names, \
+                    open(self.output_dir + 'out_PMN/'+self.retrieval_name+'_params.json', 'w'))
+            sampler = un.ReactiveNestedSampler(free_parameter_names, 
+                                               self.LogLikelihood,
+                                               self.Prior,
+                                               log_dir=self.output_dir + "out_" + self.retrieval_name, 
+                                               resume=True)
 
-            parameters_read = json.load(open(output_dir + 'out_PMN/'+ \
-                                        name+ \
-                                        '_params.json'))
-            sample_dict[name] = samples
-            parameter_dict[name] = parameters_read
-        return sample_dict, parameter_dict
-
-    def plotAll(self, output_dir = None):
-        """
-        plotAll
-        Produces plots for the best fit spectrum, a sample of 100 output spectra,
-        the best fit PT profile and a corner plot for parameters specified in the
-        run definition.
-        """
-        if output_dir is None:
-            output_dir = self.output_dir
-        sample_dict, parameter_dict = self.getSamples(output_dir)
-
-        ###########################################
-        # Plot best-fit spectrum
-        ###########################################
-        samples_use = cp.copy(sample_dict[self.retrieval_name])
-        parameters_read = cp.copy(parameter_dict[self.retrieval_name])
-        i_p = 0
-        for pp in self.parameters:
-            if self.parameters[pp].is_free_parameter:
-                for i_s in range(len(parameters_read)):
-                    if parameters_read[i_s] == self.parameters[pp].name:
-                        samples_use[:,i_p] = sample_dict[self.retrieval_name][:, i_s]
-                i_p += 1
-                
-        print("Best fit parameters")
-        i_p = 0
-        for pp in self.parameters:
-            if self.parameters[pp].is_free_parameter:
-                for i_s in range(len(parameters_read)):
-                    if parameters_read[i_s] == self.parameters[pp].name:
-                        print(self.parameters[pp].name, samples_use[best_fit_index][i_p])
-                        i_p += 1
-                        
-        # Plotting
-        self.plotSpectra(samples_use,parameters_read)
-        self.plotSampled(samples_use, parameters_read)
-        self.plotPT(sample_dict,parameters_read)
-        self.plotCorner(sample_dict,parameter_dict,parameters_read)
-        print("Done!")
+            result = sampler.run()
+            sampler.print_results()
         return
 
     def setupData(self):
@@ -317,6 +272,17 @@ class Retrieval:
                 dd.pRT_object = rt_object
         return
 
+    def Prior(self, cube, ndim, nparams):
+        """
+        Prior
+        pymultinest prior function. Transforms unit hypercube into physical space.
+        """
+        i_p = 0
+        for pp in self.parameters:
+            if self.parameters[pp].is_free_parameter:
+                cube[i_p] = self.parameters[pp].get_param_uniform(cube[i_p])
+                i_p += 1
+
     def LogLikelihood(self,cube,ndim,nparam):
         """
         LogLikelihood
@@ -365,7 +331,7 @@ class Retrieval:
                                             self.plotting)
                 # Save sampled outputs if necessary.
                 if self.run_mode == 'evaluate':
-                    np.savetxt(self.output_dir + 'evaluate_data/model_spec_best_fit'+ 
+                    np.savetxt(self.output_dir + 'evaluate_' + self.retrieval_name + '/model_spec_best_fit_'+ 
                             name+'.dat', 
                             np.column_stack((wlen_model, 
                                                 spectrum_model)))
@@ -388,16 +354,42 @@ class Retrieval:
         #print(log_likelihood)
         return log_likelihood + log_prior  
     
-    def Prior(self, cube, ndim, nparams):
+    def getSamples(self, output_dir = None):
         """
-        Prior
-        pymultinest prior function. Transforms unit hypercube into physical space.
+        getSamples
+        This function looks in the given output directory and finds the post_equal_weights
+        file associated with the current retrieval name.
+
+        parameters
+        ----------
+        output_dir : str
+            Parent directory of the out_PMN/*post_equal_weights.dat file
+        
+        returns
+        -------
+        sample_dict : dict
+            A dictionary with keys being the name of the retrieval, and values are a numpy
+            ndarray containing the samples in the post_equal_weights file
+        parameter_dict : dict
+            A dictionary with keys being the name of the retrieval, and values are a list of names
+            of the parameters used in the retrieval. The first name corresponds to the first column
+            of the samples, and so on.
         """
-        i_p = 0
-        for pp in self.parameters:
-            if self.parameters[pp].is_free_parameter:
-                cube[i_p] = self.parameters[pp].get_param_uniform(cube[i_p])
-                i_p += 1
+        sample_dict = {}
+        parameter_dict = {}
+        if output_dir is None:
+            output_dir = self.output_dir
+        for name in self.corner_files:
+            samples = np.genfromtxt(output_dir +'out_PMN/'+ \
+                                    name+ \
+                                    '_post_equal_weights.dat')
+
+            parameters_read = json.load(open(output_dir + 'out_PMN/'+ \
+                                        name+ \
+                                        '_params.json'))
+            sample_dict[name] = samples
+            parameter_dict[name] = parameters_read
+        return sample_dict, parameter_dict
 
     def getBestFitParams(self,best_fit_params,parameters_read):
         """
@@ -496,6 +488,50 @@ class Retrieval:
         self.best_fit_specs[ret_name]= [bf_wlen,bf_spectrum]
         return bf_wlen, bf_spectrum
 
+
+#############################################################
+# Plotting functions
+#############################################################
+    def plotAll(self, output_dir = None):
+        """
+        plotAll
+        Produces plots for the best fit spectrum, a sample of 100 output spectra,
+        the best fit PT profile and a corner plot for parameters specified in the
+        run definition.
+        """
+        if output_dir is None:
+            output_dir = self.output_dir
+        sample_dict, parameter_dict = self.getSamples(output_dir)
+
+        ###########################################
+        # Plot best-fit spectrum
+        ###########################################
+        samples_use = cp.copy(sample_dict[self.retrieval_name])
+        parameters_read = cp.copy(parameter_dict[self.retrieval_name])
+        i_p = 0
+        for pp in self.parameters:
+            if self.parameters[pp].is_free_parameter:
+                for i_s in range(len(parameters_read)):
+                    if parameters_read[i_s] == self.parameters[pp].name:
+                        samples_use[:,i_p] = sample_dict[self.retrieval_name][:, i_s]
+                i_p += 1
+                
+        print("Best fit parameters")
+        i_p = 0
+        for pp in self.parameters:
+            if self.parameters[pp].is_free_parameter:
+                for i_s in range(len(parameters_read)):
+                    if parameters_read[i_s] == self.parameters[pp].name:
+                        print(self.parameters[pp].name, samples_use[best_fit_index][i_p])
+                        i_p += 1
+                        
+        # Plotting
+        self.plotSpectra(samples_use,parameters_read)
+        self.plotSampled(samples_use, parameters_read)
+        self.plotPT(sample_dict,parameters_read)
+        self.plotCorner(sample_dict,parameter_dict,parameters_read)
+        print("Done!")
+        return
     def plotSpectra(self,samples_use,parameters_read,model_generating_func = None):
         """
         plotSpectra
@@ -630,6 +666,9 @@ class Retrieval:
         # weird scaling to get axis to look ok on log plots
         if self.rd.plot_kwargs["xscale"] == 'log':
             lims = [lims[0]*1.09,lims[1]*1.02]
+        else:
+            lims = [bf_wlen[0]*0.98,bf_wlen[-1]*1.02]
+        ax.set_xlim(lims)
         ax_r.set_xlim(lims)
         ax_r.set_ylim(ymin=-yabs_max, ymax=yabs_max)
         ax_r.fill_between(lims,-1,1,color='dimgrey',alpha=0.4,zorder = -10)
@@ -676,12 +715,12 @@ class Retrieval:
             ax_r.xaxis.set_minor_formatter(NullFormatter())
         else:
             ax_r.xaxis.set_minor_locator(AutoMinorLocator())
-            ax._rtick_params(axis='both', which='minor', bottom=True, top=True, left=True, right=True, direction='in',length=5)
+            ax_r.tick_params(axis='both', which='minor', bottom=True, top=True, left=True, right=True, direction='in',length=5)
         ax_r.yaxis.set_minor_locator(AutoMinorLocator())
         ax_r.tick_params(axis='both', which='minor', bottom=True, top=True, left=True, right=True, direction='in',length=5)
         ax_r.set_ylabel("Residuals [$\sigma$]")
         ax_r.set_xlabel(self.rd.plot_kwargs["spec_xlabel"])
-        ax.legend(loc='best').set_zorder(1002) 
+        ax.legend(loc='upper center',ncol = len(self.data.keys())+1).set_zorder(1002) 
         plt.tight_layout()
         plt.savefig(self.output_dir + 'evaluate_'+self.rd.retrieval_name +'/best_fit_spec.pdf')
         return fig, ax, ax_r
@@ -696,14 +735,21 @@ class Retrieval:
         samples_use : np.ndarray
             posterior samples from pynmultinest outputs (post_equal_weights)
         """
-        print("Plotting Best-fit spectrum with 100 samples")
+        print("Plotting Best-fit spectrum with "+ str(self.rd.plot_kwargs["nsample"]) + " samples.")
+        print("This could take some time...")
         len_samples = samples_use.shape[0]
         path = self.output_dir + 'evaluate_'+self.retrieval_name + "/"
+
+        data_use= {}
+        for name, dd in self.data.items():
+            if not os.path.exists(path + name.replace(' ','_')+'_sampled_'+ 
+                        str(int(self.rd.plot_kwargs["nsample"])).zfill(int(np.log10(self.rd.plot_kwargs["nsample"])+1))+'.dat'):
+                data_use[name] = dd
 
         for i_sample in range(int(self.rd.plot_kwargs["nsample"])):
             random_index = int(np.random.uniform()*len_samples)
             self.LogLikelihood(samples_use[random_index, :-1], 0, 0)
-            for name,dd in self.data.items():
+            for name,dd in data_use.items():
                 if dd.external_pRT_reference == None:
                     np.savetxt(path +name.replace(' ','_')+'_sampled_'+ 
                                 str(int(i_sample+1)).zfill(int(np.log10(self.rd.plot_kwargs["nsample"])+1))+'.dat',
@@ -711,11 +757,9 @@ class Retrieval:
                                                  self.posterior_sample_specs[name][1])))
             
         fig,ax = plt.subplots(figsize = (16,10))
-        try : 
-            plot_specs(fig,ax,path, 'Retrieved', '#ff9f9f', '#ff3d3d', -10, rebin_val = 5)
-        except : 
-            print("Plotting sample spectrum failed, are you sure you allowed sampling?")
-            return None
+        plot_specs(fig,ax,path, 'Retrieved', '#ff9f9f', '#ff3d3d', -10, rebin_val = 5)
+        #print("Plotting sample spectrum failed, are you sure you allowed sampling?")
+        #return None
         
         for name,dd in self.data.items():
             plot_data(fig,ax,dd, name, 'white', 0, rebin_val = 5)
@@ -774,7 +818,7 @@ class Retrieval:
                         x1 = temps_sort[int(len_samp*(0.5-0.997/2.)), :], \
                         x2 = temps_sort[int(len_samp*(0.5+0.997/2.)), :], \
                         color = 'brown', label = '3 sig')
-        ax.axhline.fill_betweenx(pressures, \
+        ax.fill_betweenx(pressures, \
                         x1 = temps_sort[int(len_samp*(0.5-0.95/2.)), :], \
                         x2 = temps_sort[int(len_samp*(0.5+0.95/2.)), :], \
                         color = 'orange', label = '2 sig')