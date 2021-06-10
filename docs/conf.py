15d547a3b8d45947453f5be1d81eda51a8a0c22a
Evert Nasedkin <nasedkinevert@gmail.com>
2021-06-10 11:30:43 +0200

    Updated conf.py with RadTRANS, reformatted example notebook to match style.

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../'))
 autodoc_mock_imports = ['petitRADTRANS.fort_input', 'petitRADTRANS.fort_spec', \
                         'petitRADTRANS.fort_rebin', 'numpy', 'petitRADTRANS.nat_cst', 'scipy', 'glob', \
-                        'os', 'sys', 'pdb', 'h5py']
+                        'os', 'sys', 'pdb', 'h5py', 'petitRADTRANS.Radtrans']
 
 # -- Project information -----------------------------------------------------
 
