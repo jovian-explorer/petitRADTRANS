Better CFF S/N calculation and fix Planet name-related issues.

petitRADTRANS/ccf/ccf.py
@@ -38,29 +38,36 @@ def ccf_analysis(wavelengths, observed_spectrum, modelled_spectrum, velocity_ran
     velocities = np.linspace(
         -(np.max(wavelengths) - np.min(wavelengths)) / np.mean(wavelengths) * nc.c * 1e-5,
         (np.max(wavelengths) - np.min(wavelengths)) / np.mean(wavelengths) * nc.c * 1e-5,
-        len(ccf)
+        np.size(ccf, axis=1)
     )
 
-    index = np.abs(velocities) < velocity_range
-    velocities = velocities[index]
+    wh = np.where(np.abs(velocities) < velocity_range)
+    velocities = velocities[wh]
 
-    snr, mu, std = calculate_ccf_snr(velocities, ccf[index])
+    snr = np.zeros(np.size(ccf, axis=0))
+    mu = np.zeros(np.size(ccf, axis=0))
+    std = np.zeros(np.size(ccf, axis=0))
 
-    log_l = -len(corrected_observed_spectrum) / 2. * np.log(
-        1. / len(corrected_observed_spectrum) * np.sum(
-            (corrected_observed_spectrum - corrected_modelled_spectrum) ** 2.
+    for i, ccf_ in enumerate(ccf):  # TODO this can be made more efficient
+        snr[i], mu[i], std[i] = calculate_ccf_snr(velocities, ccf_[wh])
+
+    log_l = -np.size(corrected_observed_spectrum) / 2. * np.log(
+        1. / np.size(corrected_observed_spectrum) * np.sum(
+            (corrected_observed_spectrum - corrected_modelled_spectrum) ** 2.,
+            axis=1
         )
     )
 
-    log_l_ccf = -len(corrected_observed_spectrum) / 2. * np.log(
-        np.std(corrected_observed_spectrum) ** 2.
-        - 2. * np.max(ccf[index][np.argmin(np.abs(velocities))]) / len(corrected_observed_spectrum)
-        + np.std(corrected_modelled_spectrum) ** 2.
+    log_l_ccf = -np.size(corrected_observed_spectrum, axis=1) / 2. * np.log(
+        np.std(corrected_observed_spectrum, axis=1) ** 2.
+        - 2. * np.max(ccf[:, wh[0]][:, np.argmin(np.abs(velocities))], axis=0)
+        / np.size(corrected_observed_spectrum, axis=1)
+        + np.std(corrected_modelled_spectrum, axis=1) ** 2.
     )
 
-    cross_correlation = (ccf[index] - mu) / std
+    cross_correlation = (np.transpose(ccf[:, wh[0]]) - mu) / std
 
-    return snr, velocities, cross_correlation, log_l, log_l_ccf
+    return snr, velocities, np.transpose(cross_correlation), log_l, log_l_ccf
 
 
 def calculate_ccf_snr(xval, signal):
@@ -76,9 +83,9 @@ def calculate_ccf_snr(xval, signal):
         mu: the mean value of the CCF's noise
         std: the standard  deviation of the CCF noise
     """
-    index = np.abs(xval) > 50.  # assumes the peak is within -50, +50
+    index = np.where(np.abs(xval) > 50.)  # assumes the peak is within -50, +50
     mu, std = norm.fit(signal[index])  # fit of the CCF noise
-    signal_peak = signal[np.argmin(np.abs(xval))]  # assumes the peak is at/near 0
+    signal_peak = signal[np.argmin(np.abs(xval))]  # assumes the peak is at/near 0 # TODO this method to get the peak is not always accurate!
     snr = (signal_peak - mu) / std
 
     return snr, mu, std
@@ -96,21 +103,36 @@ def remove_large_scale_trends(freq, flux, ran=2 * 0.0015 * 1e-4):  # TODO better
     Returns:
         flux_transform: the flux of the spectrum, removed from its large scale trends
     """
+    if np.ndim(flux) == 1:
+        flux = np.array([flux])
+
     wavelength_range = np.arange(np.min(freq) - 2. * ran, np.max(freq) + 2. * ran, ran)
 
-    vals = np.zeros_like(wavelength_range[:-1])
+    vals = np.zeros_like(wavelength_range[:-1]) * np.nan
 
     for i in range(1, len(wavelength_range) - 2):
-        try:  # TODO fix warning occurring here
-            vals[i] = np.mean(flux[np.where(
-                np.logical_and(freq > wavelength_range[i], freq < wavelength_range[i + 1])
-            )])
-        except RuntimeWarning:
-            vals[i] = 0.
+        wh = np.where(
+                np.logical_and(freq >= wavelength_range[i], freq < wavelength_range[i + 1])
+            )
+
+        if np.size(wh) > 0:
+            vals[i] = np.mean(flux[:, wh])
+        else:
+            vals[i] = vals[i - 1]
 
     vals[0], vals[-1] = vals[1], vals[-2]  # expand
+
+    # Remove leading and tailing NaNs
+    wh = np.where(np.logical_not(np.isnan(vals)))[0]
+    vals[:wh[0]] = vals[wh[0]]
+    vals[wh[-1]:] = vals[wh[-1]]
+
+    # Interpolate means and divide to the flux
     taut_val = interp1d((wavelength_range[1:] + wavelength_range[:-1]) / 2., vals)
     flux_transform = flux / taut_val(freq) - 1.
-    flux_transform[np.isnan(flux_transform)] = 0.
+
+    # Remove last remaining NaNs (there should be none)
+    wh = np.where(np.isnan(flux_transform))
+    flux_transform[wh] = 0.
 
     return flux_transform

petitRADTRANS/ccf/ccf_utils.py
@@ -151,7 +151,8 @@ def generate_phoenix_star_spectrum_file(star_spectrum_file, star_effective_tempe
 def get_ccf_results(band, star_snr, settings, models, instrument_resolving_power, pixel_sampling,
                     species_list, velocity_range,
                     observing_time=1., transit_duration=None,
-                    star_snr_reference_apparent_magnitude=None, star_apparent_magnitude=None):
+                    star_snr_reference_apparent_magnitude=None, star_apparent_magnitude=None,
+                    mock_observation_number=1):
     if transit_duration is None:
         transit_duration = 0.5 * observing_time
 
@@ -199,7 +200,8 @@ def get_ccf_results(band, star_snr, settings, models, instrument_resolving_power
                         transit_duration=transit_duration,
                         instrument_resolving_power=instrument_resolving_power,
                         pixel_sampling=pixel_sampling,
-                        instrument_wavelength_range=wrange * 1e-4
+                        instrument_wavelength_range=wrange * 1e-4,
+                        number=mock_observation_number
                     )
 
                 for species in species_list:
@@ -220,6 +222,7 @@ def get_ccf_results(band, star_snr, settings, models, instrument_resolving_power
                     snr, velocity, cross_correlation, log_l, log_l_ccf = ccf_analysis(
                         wlen_out, observed_spectrum, single_rebinned
                     )
+                    print(band, setting, order, detector, species)
 
                     # Add the CCF of each order and each detector to retrieve the CCF of one setting
                     f = interp1d(velocity, cross_correlation)
@@ -244,16 +247,21 @@ def get_ccf_results(band, star_snr, settings, models, instrument_resolving_power
                             else:
                                 raise ValueError(str(error_msg))
 
-
             for species in species_list:
                 if species == 'all':
                     continue
 
-                snr, mu, std = calculate_ccf_snr(x[species], add[species])
+                snr = np.zeros(mock_observation_number)
+                mu = np.zeros(mock_observation_number)
+                std = np.zeros(mock_observation_number)
+
+                for j in range(mock_observation_number):
+                    snr[j], mu[j], std[j] = calculate_ccf_snr(x[species], add[species][j, :])
+
                 results[band][setting][species] = {
                     'S/N': snr,
                     'velocity': x[species],
-                    'CCF': (add[species] - mu) / std,
+                    'CCF': (np.transpose(add[species]) - mu) / std,
                     'LogL': log_l_ccf_all_orders[species]
                 }
 
@@ -356,7 +364,8 @@ def get_tsm_snr_pcloud(band, wavelength_boundaries, star_distances, p_clouds, mo
                        exposure_time, telescope_mirror_radius, telescope_throughput,
                        instrument_resolving_power, pixel_sampling,
                        noise_correction_coefficient=1.0, scale_factor=1.0, star_snr=None,
-                       star_apparent_magnitude=None, star_snr_reference_apparent_magnitude=None):
+                       star_apparent_magnitude=None, star_snr_reference_apparent_magnitude=None,
+                       mock_observation_number=1):
     settings = {band: settings[band]}
 
     if star_apparent_magnitude is not None:
@@ -373,13 +382,17 @@ def get_tsm_snr_pcloud(band, wavelength_boundaries, star_distances, p_clouds, mo
     tsm = np.zeros_like(tsm_variable)
 
     snrs = {}
+    snrs_error = {}
+    results = {}
 
     for setting in settings[band]:
         snrs[setting] = {}
+        snrs_error[setting] = {}
 
         for species in species_list:
             if species != 'all':
                 snrs[setting][species] = np.zeros((np.size(tsm_variable), np.size(p_clouds)))
+                snrs_error[setting][species] = np.zeros((np.size(tsm_variable), np.size(p_clouds)))
 
     j_band_boundaries = np.array([1.07, 1.4]) * 1e-4  # cm, used in TSM calculation
 
@@ -420,7 +433,7 @@ def get_tsm_snr_pcloud(band, wavelength_boundaries, star_distances, p_clouds, mo
                 band: star_snr[band]
             }
 
-        results = {}
+        results[tsm_var] = {}
 
         for j, pc in enumerate(p_clouds):
             target_model = ParametersDict(t_int, metallicity, co_ratio, pc).to_str()
@@ -442,7 +455,8 @@ def get_tsm_snr_pcloud(band, wavelength_boundaries, star_distances, p_clouds, mo
                         observing_time=exposure_time,
                         transit_duration=planet_transit_duration,
                         star_snr_reference_apparent_magnitude=star_snr_reference_apparent_magnitude,
-                        star_apparent_magnitude=star_mag
+                        star_apparent_magnitude=star_mag,
+                        mock_observation_number=mock_observation_number
                     )
 
                     model_found = True
@@ -452,11 +466,14 @@ def get_tsm_snr_pcloud(band, wavelength_boundaries, star_distances, p_clouds, mo
                 for setting in settings[band]:
                     for species in species_list:
                         if species != 'all':
-                            snrs[setting][species][i, j] = results[target_model][band][setting][species]['S/N']
+                            # Assume that the distribution of S/N is gaussian ("mostly" accurate)
+                            mu, std = norm.fit(results[target_model][band][setting][species]['S/N'])
+                            snrs[setting][species][i, j] = mu
+                            snrs_error[setting][species][i, j] = std
             else:
                 raise KeyError(f"model '{target_model}' was not found in the models dictionary")
 
-    return snrs, tsm
+    return snrs, snrs_error, tsm, results
 
 
 def load_dat(file, **kwargs):

petitRADTRANS/ccf/mock_observation.py
@@ -64,11 +64,13 @@ def convolve_rebin(input_wavelengths, input_flux,
 
 
 def generate_mock_observation(wavelengths, flux, snr_per_res_element, observing_time, transit_duration,
-                              instrument_resolving_power, pixel_sampling, instrument_wavelength_range):
+                              instrument_resolving_power, pixel_sampling, instrument_wavelength_range,
+                              number=1):
     """
     Generate a mock observation from a modelled transmission spectrum.
     The noise of the transmission spectrum is estimated assuming that to retrieve the planetary spectrum, the flux of
     the star with the planet transiting in front of it was subtracted to the flux of the star alone.
+    It is possible to generate multiple mock observations with the same setting, but a different random noise.
 
     Args:
         wavelengths: (cm) the wavelengths of the model
@@ -79,6 +81,7 @@ def generate_mock_observation(wavelengths, flux, snr_per_res_element, observing_
         instrument_resolving_power: the instrument resolving power
         pixel_sampling: the pixel sampling of the instrument
         instrument_wavelength_range: (cm) size-2 array containing the min and max wavelengths of the instrument
+        number: number of mock observations to generate
 
     Returns:
         observed_spectrum: the modelled spectrum rebinned, altered, and with a random white noise from the instrument
@@ -118,10 +121,13 @@ def generate_mock_observation(wavelengths, flux, snr_per_res_element, observing_
             (1 - full_rebinned) * observing_time * (observing_time - full_rebinned * transit_duration)
             / (transit_duration * (observing_time - transit_duration))
         )
-    observed_spectrum = full_rebinned + np.random.normal(
+
+    rng = np.random.default_rng()
+
+    observed_spectrum = full_rebinned + rng.normal(
         loc=0.,
         scale=noise_per_pixel,
-        size=len(full_rebinned)
+        size=(number, np.size(full_rebinned))
     )
 
     return observed_spectrum, full_lsf_ed, wavelengths_out, full_rebinned, snr_per_res_element