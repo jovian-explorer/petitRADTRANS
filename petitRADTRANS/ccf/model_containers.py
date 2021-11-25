Add useful Planet attributes, ESM calculation, fix contour_corner, retrieval testing, and code clean-up

petitRADTRANS/ccf/model_containers.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import pickle
 
@@ -148,6 +149,9 @@ class Planet:
             system_apparent_magnitude_j=0.,
             system_apparent_magnitude_j_error_upper=0.,
             system_apparent_magnitude_j_error_lower=0.,
+            system_apparent_magnitude_k=0.,
+            system_apparent_magnitude_k_error_upper=0.,
+            system_apparent_magnitude_k_error_lower=0.,
             system_proper_motion=0.,
             system_proper_motion_error_upper=0.,
             system_proper_motion_error_lower=0.,
@@ -282,6 +286,9 @@ class Planet:
         self.system_apparent_magnitude_j = system_apparent_magnitude_j
         self.system_apparent_magnitude_j_error_upper = system_apparent_magnitude_j_error_upper
         self.system_apparent_magnitude_j_error_lower = system_apparent_magnitude_j_error_lower
+        self.system_apparent_magnitude_k = system_apparent_magnitude_k
+        self.system_apparent_magnitude_k_error_upper = system_apparent_magnitude_k_error_upper
+        self.system_apparent_magnitude_k_error_lower = system_apparent_magnitude_k_error_lower
         self.system_proper_motion = system_proper_motion
         self.system_proper_motion_error_upper = system_proper_motion_error_upper
         self.system_proper_motion_error_lower = system_proper_motion_error_lower
@@ -417,6 +424,9 @@ class Planet:
                 'system_apparent_magnitude_j': 'None',
                 'system_apparent_magnitude_j_error_upper': 'None',
                 'system_apparent_magnitude_j_error_lower': 'None',
+                'system_apparent_magnitude_k': 'None',
+                'system_apparent_magnitude_k_error_upper': 'None',
+                'system_apparent_magnitude_k_error_lower': 'None',
                 'system_proper_motion': 'deg/s',
                 'system_proper_motion_error_upper': 'deg/s',
                 'system_proper_motion_error_lower': 'deg/s',
@@ -473,6 +483,142 @@ class Planet:
 
                 if self.units[key] != 'N/A':
                     data_set.attrs['units'] = self.units[key]
+
+    @classmethod
+    def from_tab_file(cls, filename, use_best_mass=True):
+        """Read from a NASA Exoplanet Archive Database .tab file.
+        Args:
+            filename: file to read
+            use_best_mass: if True, use NASA Exoplanet Archive Database 'bmass' argument instead of 'mass'.
+
+        Returns:
+            planets: a list of Planet objects
+        """
+        with open(filename, 'r') as f:
+            line = f.readline()
+            line = line.strip()
+
+            # Skip header
+            while line[0] == '#':
+                line = f.readline()
+                line = line.strip()
+
+            # Read column names
+            columns_name = line.split('\t')
+
+            planet_name_index = columns_name.index('pl_name')
+
+            planets = {}
+
+            # Read data
+            for line in f:
+                line = line.strip()
+                columns = line.split('\t')
+
+                new_planet = cls(columns[planet_name_index])
+                keys = []
+
+                for i, value in enumerate(columns):
+                    # Clearer keynames
+                    keys.append(columns_name[i])
+
+                    if value != '':
+                        try:
+                            value = float(value)
+                        except ValueError:
+                            pass
+
+                        value, keys[i] = Planet.__convert_nasa_exoplanet_archive(
+                            value, keys[i], use_best_mass=use_best_mass
+                        )
+                    else:
+                        value = None
+
+                    if keys[i] in new_planet.__dict__:
+                        new_planet.__dict__[keys[i]] = value
+
+                # Try to calculate the planet mass and radius if missing
+                if new_planet.radius == 0 and new_planet.mass > 0 and new_planet.density > 0:
+                    new_planet.radius = (3 * new_planet.mass / (4 * np.pi * new_planet.density)) ** (1 / 3)
+
+                    partial_derivatives = np.array([
+                        new_planet.radius / (3 * new_planet.mass),  # dr/dm
+                        - new_planet.radius / (3 * new_planet.density)  # dr/drho
+                    ])
+                    uncertainties = np.abs(np.array([
+                        [new_planet.mass_error_lower, new_planet.mass_error_upper],
+                        [new_planet.density_error_lower, new_planet.density_error_upper]
+                    ]))
+
+                    new_planet.radius_error_lower, new_planet.radius_error_upper = \
+                        calculate_uncertainty(partial_derivatives, uncertainties)  # lower and upper errors
+                elif new_planet.mass == 0 and new_planet.radius > 0 and new_planet.density > 0:
+                    new_planet.mass = new_planet.density * 4 / 3 * np.pi * new_planet.radius ** 3
+
+                    partial_derivatives = np.array([
+                        new_planet.mass / new_planet.density,  # dm/drho
+                        3 * new_planet.radius / new_planet.radius  # dm/dr
+                    ])
+                    uncertainties = np.abs(np.array([
+                        [new_planet.density_error_lower, new_planet.density_error_upper],
+                        [new_planet.radius_error_lower, new_planet.radius_error_upper]
+                    ]))
+
+                    new_planet.mass_error_lower, new_planet.mass_error_upper = \
+                        calculate_uncertainty(partial_derivatives, uncertainties)  # lower and upper errors
+
+                # Try to calculate the star radius if missing
+                if new_planet.star_radius == 0 and new_planet.star_mass > 0:
+                    if new_planet.star_surface_gravity > 0:
+                        new_planet.star_radius, \
+                            new_planet.star_radius_error_upper, new_planet.star_radius_error_lower = \
+                            new_planet.surface_gravity2radius(
+                                new_planet.star_surface_gravity,
+                                new_planet.star_mass,
+                                surface_gravity_error_upper=new_planet.star_surface_gravity_error_upper,
+                                surface_gravity_error_lower=new_planet.star_surface_gravity_error_lower,
+                                mass_error_upper=new_planet.star_mass_error_upper,
+                                mass_error_lower=new_planet.star_mass_error_lower
+                            )
+                    elif new_planet.star_density > 0:
+                        new_planet.star_radius = \
+                            (3 * new_planet.star_mass / (4 * np.pi * new_planet.star_density)) ** (1 / 3)
+
+                        partial_derivatives = np.array([
+                            new_planet.star_radius / (3 * new_planet.star_mass),  # dr/dm
+                            - new_planet.star_radius / (3 * new_planet.star_density)  # dr/drho
+                        ])
+                        uncertainties = np.abs(np.array([
+                            [new_planet.star_mass_error_lower, new_planet.star_mass_error_upper],
+                            [new_planet.star_density_error_lower, new_planet.star_density_error_upper]
+                        ]))
+
+                        new_planet.star_radius_error_lower, new_planet.star_radius_error_upper = \
+                            calculate_uncertainty(partial_derivatives, uncertainties)  # lower and upper errors
+
+                if 'surface_gravity' not in keys and new_planet.radius > 0 and new_planet.mass > 0:
+                    new_planet.surface_gravity, \
+                        new_planet.surface_gravity_error_upper, new_planet.surface_gravity_error_lower = \
+                        new_planet.mass2surface_gravity(
+                            new_planet.mass,
+                            new_planet.radius,
+                            mass_error_upper=new_planet.mass_error_upper,
+                            mass_error_lower=new_planet.mass_error_lower,
+                            radius_error_upper=new_planet.radius_error_upper,
+                            radius_error_lower=new_planet.radius_error_lower
+                        )
+
+                if 'equilibrium_temperature' not in keys \
+                        and new_planet.orbit_semi_major_axis > 0 \
+                        and new_planet.star_effective_temperature > 0 \
+                        and new_planet.star_radius > 0:
+                    new_planet.equilibrium_temperature, \
+                        new_planet.equilibrium_temperature_error_upper, new_planet.equilibrium_temperature_error_lower = \
+                        new_planet.calculate_planetary_equilibrium_temperature()
+
+                planets[new_planet.name] = new_planet
+
+        return planets
 
     @classmethod
     def from_votable(cls, votable):
@@ -564,7 +710,7 @@ class Planet:
         return new_planet
 
     @staticmethod
-    def __convert_nasa_exoplanet_archive(value, key, verbose=False):
+    def __convert_nasa_exoplanet_archive(value, key, verbose=False, use_best_mass=False):
         skip_unit_conversion = False
 
         # Heads
@@ -668,10 +814,17 @@ class Planet:
             if not skip_unit_conversion:
                 value *= nc.m_jup
         elif '_massj' in key:
-            key = key.replace('_massj', '_mass')
+            if not use_best_mass:
+                key = key.replace('_massj', '_mass')
 
-            if not skip_unit_conversion:
-                value *= nc.m_jup
+                if not skip_unit_conversion:
+                    value *= nc.m_jup
+        elif '_bmassj' in key:
+            if use_best_mass:
+                key = key.replace('_bmassj', '_mass')
+
+                if not skip_unit_conversion:
+                    value *= nc.m_jup
         elif '_teff' in key:
             key = key.replace('_teff', '_effective_temperature')
         elif '_met' in key:
@@ -855,6 +1008,38 @@ class Planet:
         errors = calculate_uncertainty(partial_derivatives, uncertainties)  # lower and upper errors
 
         return surface_gravity, errors[1], -errors[0]
+
+    @staticmethod
+    def surface_gravity2radius(surface_gravity, mass,
+                               surface_gravity_error_upper=0., surface_gravity_error_lower=0.,
+                               mass_error_upper=0., mass_error_lower=0.):
+        """
+        Convert the mass of a planet to its surface gravity.
+        Args:
+            surface_gravity: (cm.s-2) surface_gravity of the planet
+            mass: (g) mass of the planet
+            mass_error_upper: (g) upper error on the planet mass
+            mass_error_lower: (g) lower error on the planet mass
+            surface_gravity_error_upper: (cm.s-2) upper error on the planet radius
+            surface_gravity_error_lower: (cm.s-2) lower error on the planet radius
+
+        Returns:
+            (cm.s-2) the surface gravity of the planet, and its upper and lower error
+        """
+        radius = (nc.G * mass / surface_gravity) ** 0.5
+
+        partial_derivatives = np.array([
+            radius / (2 * mass),  # dr/dm
+            - radius / (2 * surface_gravity)  # dr/dg
+        ])
+        uncertainties = np.abs(np.array([
+            [mass_error_lower, mass_error_upper],
+            [surface_gravity_error_lower, surface_gravity_error_upper]
+        ]))
+
+        errors = calculate_uncertainty(partial_derivatives, uncertainties)  # lower and upper errors
+
+        return radius, errors[1], -errors[0]
 
 
 class SimplePlanet(Planet):

petitRADTRANS/retrieval/plotting.py
@@ -141,6 +141,13 @@ def contour_corner(sampledict,
             kwarg must be a dictionary with the arguments as keys and values as the values.
     """
     import matplotlib as mpl
+
+    if parameter_ranges is None:
+        parameter_ranges = {}
+
+    if parameter_plot_indices is None:
+        parameter_plot_indices = {}
+
     if prt_plot_style:
         mpl.rcParams.update(mpl.rcParamsDefault)
         font = {'family': 'serif'}
@@ -163,6 +170,7 @@ def contour_corner(sampledict,
                       "#B429FF"]
     else:
         mpl.rcParams.update(mpl.rcParamsDefault)
+        color_list = [f'C{i}' for i in range(8)]  # standard matplotlib color cycle
 
         # from .plot_style import prt_colours
     # color_list = prt_colours
@@ -179,12 +187,14 @@ def contour_corner(sampledict,
 
         n_samples = len(samples)
         s = n_samples
-        try:
-            if parameter_plot_indices[key] is None:
-                parameter_plot_indices = {key: np.linspace(0, len(parameter_names[key]) - 1,
-                                                           len(parameter_names[key]) - 1).astype('int')}
-        except:
-            pass
+
+        if key not in parameter_plot_indices:
+            parameter_plot_indices[key] = range(len(parameter_names[key]))
+        elif parameter_plot_indices[key] is None:  # same as in the case the key doesn't exists
+            parameter_plot_indices[key] = range(len(parameter_names[key]))
+
+        if key not in parameter_ranges:
+            parameter_ranges[key] = [None] * (max(parameter_plot_indices[key]) + 1)
 
         data_list = []
         labels_list = []

petitRADTRANS/retrieval/retrieval.py
@@ -112,7 +112,7 @@ class Retrieval:
         self.param_dict = {}
         # Set up pretty plotting
         if pRT_plot_style:
-            import petitRADTRANS.retrieval.plot_style
+            import petitRADTRANS.retrieval.plot_style  # TODO improve so that it doesn't break matplotlib
         self.prt_plot_style = pRT_plot_style
         # Path to input opacities
         self.path = petitradtrans_config['Paths']['pRT_input_data_path']
@@ -195,8 +195,10 @@ class Retrieval:
                                 resume,
                                 max_iter)
             return
+
         if const_efficiency_mode and sampling_efficiency > 0.1:
             logging.warning("Sampling efficiency should be ~ 0.05 if you're using constant efficiency mode!")
+
         prefix = self.output_dir + 'out_PMN/' + self.retrieval_name + '_'
 
         if len(self.output_dir + 'out_PMN/') > 100:
@@ -302,7 +304,7 @@ class Retrieval:
                     free_parameter_names.append(self.parameters[pp].name)
                     n_params += 1
 
-            if max_iter is 0:
+            if max_iter == 0:
                 max_iter = None
 
             sampler = un.ReactiveNestedSampler(free_parameter_names,
@@ -340,7 +342,6 @@ class Retrieval:
                 A Pymultinest stats dictionary, from Analyzer.get_stats().
                 This contains the evidence and best fit parameters.
         """
-
         with open(
                 self.output_dir + "evaluate_" + self.retrieval_name + "/" + self.retrieval_name + "_ret_summary.txt",
                 "w+"
@@ -674,11 +675,11 @@ class Retrieval:
                 of the parameters used in the retrieval. The first name corresponds to the first column
                 of the samples, and so on.
         """
-
+        # TODO could be static
         if ret_names is None:
             ret_names = []
         if output_dir is None:
-            output_dir = self.output_dir
+            output_dir = self.output_dir  # TODO output dir should not have a "/" in it
         if self.ultranest:
             for name in self.corner_files:
                 samples = np.genfromtxt(output_dir + 'out_' + name + '/chains/equal_weighted_post.txt')
@@ -938,8 +939,8 @@ class Retrieval:
                 params[pp] = Parameter(pp, False, value=self.parameters[pp].value)
         return params
 
-    def sample_teff(self,sample_dict,param_dict,ret_names = None,nsample = None,resolution=40):
-        """
+    def sample_teff(self, sample_dict, param_dict, ret_names=None, nsample=None, resolution=40):
+        r"""
         This function samples the outputs of a retrieval and computes Teff
         for each sample. For each sample, a model is computed at low resolution,
         and integrated to find the total radiant emittance, which is converted into

tests/test_retrieval.py
@@ -5,20 +5,15 @@ C.f. (https://petitradtrans.readthedocs.io/en/latest/content/notebooks/getting_s
 """
 import json
 import os
-# To not have numpy start parallelizing on its own
-os.environ["OMP_NUM_THREADS"] = "1"
+
+os.environ["OMP_NUM_THREADS"] = "1"  # to not have numpy start parallelizing on its own
 import numpy as np
 
-# Import Prior functions, if necessary.
 from petitRADTRANS.retrieval.util import gaussian_prior
-# Import atmospheric model function
-# You could also write your own!
-from petitRADTRANS.retrieval.models import emission_model_diseq
 from petitRADTRANS.retrieval.util import calc_MMW
 
 from .context import petitRADTRANS
-from .utils import compare_from_reference_file, tests_results_directory, \
-    reference_filenames, radtrans_parameters, temperature_guillot_2010, temperature_isothermal
+from .utils import tests_results_directory, reference_filenames, radtrans_parameters
 
 
 relative_tolerance = 1e-6  # relative tolerance when comparing with older results
@@ -37,15 +32,15 @@ def init_run():
 
     # Fixed parameters
     run_definition_simple.add_parameter(
-        'Rstar',  # name
-        False,  # is_free_parameter, So Rstar is not retrieved here.
+        'Rstar',
+        False,
         value=radtrans_parameters['stellar_parameters']['radius'] * petitRADTRANS.nat_cst.r_sun
     )
 
     # Log of the surface gravity
     run_definition_simple.add_parameter(
         'log_g',
-        False,  # is_free_parameter: we are retrieving log(g) here
+        False,
         value=np.log10(radtrans_parameters['planetary_parameters']['surface_gravity'])
     )
 
@@ -103,10 +98,10 @@ def init_run():
         radtrans_parameters['spectrum_parameters']['line_species_correlated_k'],
         eq=False,
         abund_lim=(
-            np.mean(radtrans_parameters['retrieval_parameters']['log10_species_mass_fractions_bounds']),
-            (radtrans_parameters['retrieval_parameters']['log10_species_mass_fractions_bounds'][1]
-             - radtrans_parameters['retrieval_parameters']['log10_species_mass_fractions_bounds'][0]) / 2
-        )
+            radtrans_parameters['retrieval_parameters']['log10_species_mass_fractions_bounds'][0],
+            radtrans_parameters['retrieval_parameters']['log10_species_mass_fractions_bounds'][1]
+            - radtrans_parameters['retrieval_parameters']['log10_species_mass_fractions_bounds'][0]
+        )  # prior: min = abund_lim[0], max = min + abund_lim[1]
     )
 
     # Load data
@@ -180,7 +175,7 @@ def retrieval_model_spec_iso(prt_object, parameters, pt_plot_mode=None, AMR=Fals
             Computed transmission spectrum R_pl**2/Rstar**2
     """
     # Make the P-T profile
-    pressures = prt_object.press / 1e6  # bar to cgs
+    pressures = prt_object.press * 1e-6  # bar to cgs
     temperatures = parameters['Temperature'].value * np.ones_like(pressures)
 
     # Make the abundance profiles