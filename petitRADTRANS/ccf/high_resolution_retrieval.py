Fix line-by-line sampling and planet proper motion unit conversion

petitRADTRANS/ccf/high_resolution_retrieval.py
@@ -284,6 +284,7 @@ def _get_secondary_eclipse_retrieval_model(prt_object, parameters, pt_plot_mode=
     if apply_pipeline:
         spectrum_model = simple_pipeline(
             spectral_data=spectrum_model0,
+            wavelengths=parameters['wavelengths_instrument'].value,
             airmass=parameters['airmass'].value,
             data_uncertainties=parameters['data_uncertainties'].value,
             apply_throughput_removal=True,
@@ -340,6 +341,7 @@ def _get_transit_retrieval_model(prt_object, parameters, pt_plot_mode=None, AMR=
     if apply_pipeline:
         spectrum_model = simple_pipeline(
             spectral_data=spectrum_model0,
+            wavelengths=parameters['wavelengths_instrument'].value,
             airmass=parameters['airmass'].value,
             data_uncertainties=parameters['data_uncertainties'].value,
             apply_throughput_removal=False
@@ -814,6 +816,7 @@ def init_mock_observations(planet, line_species_str, mode,
     reduced_mock_observations, reduction_matrix, reduced_uncertainties = simple_pipeline(
         spectral_data=mock_observations,
         data_uncertainties=uncertainties,
+        wavelengths=wavelengths_instrument,
         airmass=airmass,
         full=True,
         apply_throughput_removal=apply_throughput_removal,
@@ -846,18 +849,21 @@ def init_mock_observations(planet, line_species_str, mode,
     ts = copy.copy(true_spectra)
     ts = np.ma.masked_where(mock_observations.mask, ts)
     fmt, mr0t, _ = simple_pipeline(
-        ts, airmass=airmass, data_uncertainties=true_parameters['data_uncertainties'].value, full=True,
+        ts, airmass=airmass, wavelengths=wavelengths_instrument,
+        data_uncertainties=true_parameters['data_uncertainties'].value, full=True,
         apply_throughput_removal=apply_throughput_removal,
         apply_telluric_lines_removal=apply_telluric_lines_removal
     )
     w, r = retrieval_model(model, true_parameters)
 
     fmtd, mr0td, _ = simple_pipeline(ts * true_parameters['deformation_matrix'].value, airmass=airmass,
+                                     wavelengths=wavelengths_instrument,
                                      data_uncertainties=true_parameters['data_uncertainties'].value,
                                      apply_throughput_removal=apply_throughput_removal,
                                      apply_telluric_lines_removal=apply_telluric_lines_removal,
                                      full=True)
     fs, mr, _ = simple_pipeline(ts * true_parameters['deformation_matrix'].value + noise, airmass=airmass,
+                                wavelengths=wavelengths_instrument,
                                 data_uncertainties=true_parameters['data_uncertainties'].value, full=True,
                                 apply_throughput_removal=apply_throughput_removal,
                                 apply_telluric_lines_removal=apply_telluric_lines_removal,

petitRADTRANS/ccf/model_containers.py
@@ -1511,7 +1511,7 @@ class Planet:
                 key = key[:i] + '_proper_motion_' + key[i + len('_pm'):]
 
             if not skip_unit_conversion:
-                value *= 3.6e-6 / nc.snc.year
+                value *= np.deg2rad(1e-3 / 3600 / nc.snc.year)
 
         elif key == 'hostname':
             key = 'host_name'

petitRADTRANS/ccf/pipeline.py
@@ -594,7 +594,10 @@ def remove_throughput_fit(spectral_data, reduction_matrix, wavelengths, data_unc
 
     # Correction
     for i, det in enumerate(spectral_data):
-        wvl = wavelengths[i, 0, :]
+        if np.ndim(wavelengths) == 2:
+            wvl = wavelengths[i, :]
+        else:
+            wvl = wavelengths[i, 0, :]
 
         # Fit each observation
         for j, observation in enumerate(det):