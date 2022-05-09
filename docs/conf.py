5d45b683932cc069aadf794e267a168661fc4712
Paul Mollière <der.paule@gmail.com>
2022-05-09 13:44:39 +0200

    Inreased version number for PyPI.

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = u'Paul Molli\u00E8re'
 # The short X.Y version
 version = '2.4'
 # The full version, including alpha/beta/rc tags
-release = '2.4.1'
+release = '2.4.2'
 
 
 # -- General configuration ---------------------------------------------------
