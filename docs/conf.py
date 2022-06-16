d154947ca3e4b64fc5784dd75ba187da7a15d531
mauricemolli <der.paule@gmail.com>
2022-06-16 14:00:57 +0200

    Added normalization to log likelihood of retrieval package, in case error bars are scaled.

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = u'Paul Molli\u00E8re'
 # The short X.Y version
 version = '2.4'
 # The full version, including alpha/beta/rc tags
-release = '2.4.6'
+release = '2.4.7'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ def setup_function():
 
     setup(
         name='petitRADTRANS',
-        version='2.4.6',
+        version='2.4.7',
         description='Exoplanet spectral synthesis tool for retrievals',
         long_description=open(os.path.join(
           os.path.dirname(__file__), 'README.rst')).read(),
