896065c2664f164c508db62867a8687fadbfde5e
Nicholas Evert Nasedkin <nnas@vera01.bc.rzg.mpg.de>
2022-06-20 13:45:51 +0200

    Simple PT model update

--- petitRADTRANS/retrieval/models.py
+++ petitRADTRANS/retrieval/models.py
@@ -183,6 +183,176 @@ def emission_model_diseq(pRT_object,
                                   parameters['D_pl'].value)
     return wlen_model, spectrum_model
 
+def emission_model_diseq_patchy_clouds(pRT_object,
+                                       parameters,
+                                       PT_plot_mode = False,
+                                       AMR = True):
+    """
+    Disequilibrium Chemistry Emission Model
+
+    This model computes an emission spectrum based on disequilibrium carbon chemistry,
+    equilibrium clouds and a spline temperature-pressure profile. (Molliere 2020).
+
+    Args:
+        pRT_object : object
+            An instance of the pRT class, with optical properties as defined in the RunDefinition.
+        parameters : dict
+            Dictionary of required parameters:
+                *  D_pl : Distance to the planet in [cm]
+                *  log_g : Log of surface gravity
+                *  R_pl : planet radius [cm]
+                *  T_int : Interior temperature of the planet [K]
+                *  T3 : Innermost temperature spline [K]
+                *  T2 : Middle temperature spline [K]
+                *  T1 : Outer temperature spline [K]
+                *  alpha : power law index in tau = delta * press_cgs**alpha
+                *  log_delta : proportionality factor in tau = delta * press_cgs**alpha
+                *  sigma_lnorm : Width of cloud particle size distribution (log normal)
+                *  log_pquench : Pressure at which CO, CH4 and H2O abundances become vertically constant
+                *  Fe/H : Metallicity
+                *  C/O : Carbon to oxygen ratio
+                *  log_kzz : Vertical mixing parameter
+                *  fsed : sedimentation parameter
+                *  log_X_cb : Scaling factor for equilibrium cloud abundances.
+                *  patchiness : Fraction of cloud coverage
+        PT_plot_mode : bool
+            Return only the pressure-temperature profile for plotting. Evaluate mode only.
+        AMR :
+            Adaptive mesh refinement. Use the high resolution pressure grid around the cloud base.
+
+    Returns:
+        wlen_model : np.array
+            Wavlength array of computed model, not binned to data [um]
+        spectrum_model : np.array
+            Computed emission spectrum [W/m2/micron]
+    """
+    #start = time.time()
+    pglobal_check(pRT_object.press/1e6,
+                    parameters['pressure_simple'].value,
+                    parameters['pressure_scaling'].value)
+
+    #for key, val in parameters.items():
+    #    print(key,val.value)
+
+    # Priors for these parameters are implemented here, as they depend on each other
+    T3 = ((3./4.*parameters['T_int'].value**4.*(0.1+2./3.))**0.25)*(1.0-parameters['T3'].value)
+    T2 = T3*(1.0-parameters['T2'].value)
+    T1 = T2*(1.0-parameters['T1'].value)
+    delta = ((10.0**(-3.0+5.0*parameters['log_delta'].value))*1e6)**(-parameters['alpha'].value)
+
+    # Make the P-T profile
+    temp_arr = np.array([T1,T2,T3])
+    if AMR:
+        p_use = PGLOBAL
+    else:
+        p_use = pRT_object.press/1e6
+    temperatures = PT_ret_model(temp_arr, \
+                            delta,
+                            parameters['alpha'].value,
+                            parameters['T_int'].value,
+                            p_use,
+                            parameters['Fe/H'].value,
+                            parameters['C/O'].value,
+                            conv=True)
+    if PT_plot_mode:
+        return p_use, temperatures
+
+    # If in evaluation mode, and PTs are supposed to be plotted
+    abundances, MMW, small_index = get_abundances(p_use,
+                                                  temperatures,
+                                                  pRT_object.line_species,
+                                                  pRT_object.cloud_species,
+                                                  parameters,
+                                                  AMR =AMR)
+    Kzz_use = (10.0**parameters['log_kzz'].value ) * np.ones_like(p_use)
+
+    # Only include the high resolution pressure array near the cloud base.
+    pressures = p_use
+    #print(PGLOBAL.shape, pressures.shape, pressures[small_index].shape, pRT_object.press.shape)
+    if AMR:
+        #pRT_object.setup_opa_structure(PGLOBAL[small_index])
+        temperatures = temperatures[small_index]
+        pressures = PGLOBAL[small_index]
+        MMW = MMW[small_index]
+        Kzz_use = Kzz_use[small_index]
+        #pRT_object.setup_opa_structure(pressures)
+    # Calculate the spectrum
+    if pressures.shape[0] != pRT_object.press.shape[0]:
+        return None,None
+    pRT_object.press = pressures*1e6
+
+    gravity = -np.inf
+    R_pl = -np.inf
+    if 'log_g' in parameters.keys() and 'mass' in parameters.keys():
+        gravity = 10**parameters['log_g'].value
+        R_pl = np.sqrt(nc.G*parameters['mass'].value/gravity)
+    elif 'log_g' in parameters.keys():
+        gravity= 10**parameters['log_g'].value
+        R_pl = parameters['R_pl'].value
+    elif 'mass' in parameters.keys():
+        R_pl = parameters['R_pl'].value
+        gravity = nc.G * parameters['mass'].value/R_pl**2
+    else:
+        print("Pick two of log_g, R_pl and mass priors!")
+        sys.exit(5)
+    sigma_lnorm = None
+    b_hans = None
+    distribution = "lognormal"
+    if "sigma_lnorm" in parameters.keys():
+        sigma_lnorm = parameters['sigma_lnorm'].value
+    elif "b_hans" in parameters.keys():
+        b_hans = parameters['b_hans'].value
+        distribution = "hansen"
+
+    pRT_object.calc_flux(temperatures,
+                        abundances,
+                        gravity,
+                        MMW,
+                        contribution = False,
+                        fsed = parameters['fsed'].value,
+                        Kzz = Kzz_use,
+                        sigma_lnorm = sigma_lnorm,
+                        b_hans = b_hans,
+                        dist = distribution)
+    # Getting the model into correct units (W/m2/micron)
+    wlen_model = nc.c/pRT_object.freq/1e-4
+    wlen = nc.c/pRT_object.freq
+    f_lambda = pRT_object.flux*nc.c/wlen**2.
+    # convert to flux per m^2 (from flux per cm^2) cancels with step below
+    #f_lambda = f_lambda * 1e4
+    # convert to flux per micron (from flux per cm) cancels with step above
+    #f_lambda = f_lambda * 1e-4
+    # convert from ergs to Joule
+    f_lambda = f_lambda * 1e-7
+    spectrum_model_cloudy = surf_to_meas(f_lambda,
+                                  R_pl,
+                                  parameters['D_pl'].value)
+    for cloud in pRT_object.cloud_species:
+        cname = cloud.split('_')[0]
+        abundances[cname] = np.zeros_like(temperatures)
+    pRT_object.calc_flux(temperatures,
+                    abundances,
+                    gravity,
+                    MMW,
+                    contribution = False,
+                    fsed = parameters['fsed'].value,
+                    Kzz = Kzz_use,
+                    sigma_lnorm = sigma_lnorm,
+                    b_hans = b_hans,
+                    dist = distribution)
+    # Getting the model into correct units (W/m2/micron)
+    wlen_model = nc.c/pRT_object.freq/1e-4
+    wlen = nc.c/pRT_object.freq
+    f_lambda = pRT_object.flux*nc.c/wlen**2.
+    f_lambda = f_lambda * 1e-7
+    spectrum_model_clear = surf_to_meas(f_lambda,
+                                  R_pl,
+                                  parameters['D_pl'].value)
+    patchiness = parameters["patchiness"].value
+    spectrum_model = (patchiness * spectrum_model_clouds) +\
+                     ((1-patchiness)*spectrum_model_clear)
+    return wlen_model, spectrum_model
+
 def guillot_free_emission(pRT_object, \
                             parameters, \
                             PT_plot_mode = False,
@@ -415,7 +585,7 @@ def guillot_eqchem_transmission(pRT_object, \
 
     # Make the abundance profile
     COs = parameters['C/O'].value * np.ones_like(pressures)
-    FeHs = parameters['[Fe/H]'].value * np.ones_like(pressures)
+    FeHs = parameters['Fe/H'].value * np.ones_like(pressures)
 
     abundances_interp = pm.interpol_abundances(COs, \
                                                FeHs, \
@@ -451,6 +621,141 @@ def guillot_eqchem_transmission(pRT_object, \
     wlen_model = nc.c/pRT_object.freq/1e-4
     spectrum_model = (pRT_object.transm_rad/parameters['Rstar'].value)**2.
 
+    return wlen_model, spectrum_model
+
+def guillot_eqchem_emission(pRT_object, \
+                            parameters, \
+                            PT_plot_mode = False,
+                            AMR = False):
+    """
+    Equilibrium Chemistry Emission Model, Guillot Profile
+
+    This model computes a transmission spectrum based on equilibrium chemistry
+    and a Guillot temperature-pressure profile.
+
+    Args:
+        pRT_object : object
+            An instance of the pRT class, with optical properties as defined in the RunDefinition.
+        parameters : dict
+            Dictionary of required parameters:
+                *  Rstar : Radius of the host star [cm]
+                *  log_g : Log of surface gravity
+                *  R_pl : planet radius [cm]
+                *  T_int : Interior temperature of the planet [K]
+                *  T_equ : Equilibrium temperature of the planet
+                *  Fe/H : Metallicity
+                *  C/O : Carbon to oxygen ratio
+                *  Pcloud : optional, cloud base pressure of a grey cloud deck.
+        PT_plot_mode : bool
+            Return only the pressure-temperature profile for plotting. Evaluate mode only.
+        AMR :
+            Adaptive mesh refinement. Use the high resolution pressure grid around the cloud base.
+
+    Returns:
+        wlen_model : np.array
+            Wavlength array of computed model, not binned to data [um]
+        spectrum_model : np.array
+            Computed transmission spectrum R_pl**2/Rstar**2
+    """
+
+    try:
+        from petitRADTRANS import poor_mans_nonequ_chem as pm
+    except ImportError:
+        print("Could not import poor_mans_nonequ_chemistry. Exiting.")
+        sys.exit(2)
+    # Make the P-T profile
+    if AMR:
+        p_use = PGLOBAL
+    else:
+        p_use = pRT_object.press/1e6
+
+    gravity = -np.inf
+    R_pl = -np.inf
+    if 'log_g' in parameters.keys() and 'mass' in parameters.keys():
+        gravity = 10**parameters['log_g'].value
+        R_pl = np.sqrt(nc.G*parameters['mass'].value/gravity)
+    elif 'log_g' in parameters.keys():
+        gravity= 10**parameters['log_g'].value
+        R_pl = parameters['R_pl'].value
+    elif 'mass' in parameters.keys():
+        R_pl = parameters['R_pl'].value
+        gravity = nc.G * parameters['mass'].value/R_pl**2
+    else:
+        print("Pick two of log_g, R_pl and mass priors!")
+        sys.exit(5)
+
+    temperatures = nc.guillot_global(p_use, \
+                                10**parameters['log_kappa_IR'].value,
+                                parameters['gamma'].value, \
+                                gravity, \
+                                parameters['T_int'].value, \
+                                parameters['T_equ'].value)
+
+    # If in evaluation mode, and PTs are supposed to be plotted
+    if PT_plot_mode:
+        return p_use, temperatures
+
+    # Make the abundance profile
+    COs = parameters['C/O'].value * np.ones_like(p_use)
+    FeHs = parameters['Fe/H'].value * np.ones_like(p_use)
+    # If in evaluation mode, and PTs are supposed to be plotted
+    abundances, MMW, small_index = get_abundances(p_use,
+                                                  temperatures,
+                                                  pRT_object.line_species,
+                                                  pRT_object.cloud_species,
+                                                  parameters,
+                                                  AMR =AMR)
+    Kzz_use = (10.0**parameters['log_kzz'].value ) * np.ones_like(p_use)
+
+    # Only include the high resolution pressure array near the cloud base.
+    pressures = p_use
+    #print(PGLOBAL.shape, pressures.shape, pressures[small_index].shape, pRT_object.press.shape)
+    if AMR:
+        #pRT_object.setup_opa_structure(PGLOBAL[small_index])
+        temperatures = temperatures[small_index]
+        pressures = PGLOBAL[small_index]
+        MMW = MMW[small_index]
+        Kzz_use = Kzz_use[small_index]
+        #pRT_object.setup_opa_structure(pressures)
+    # Calculate the spectrum
+    #print(pressures.shape,pRT_object.press.shape[0],p_use.shape)
+    #print(temperatures.shape)
+    if pressures.shape[0] != pRT_object.press.shape[0]:
+        return None,None
+    pRT_object.press = pressures*1e6
+
+    sigma_lnorm = None
+    b_hans = None
+    distribution = "lognormal"
+    if "sigma_lnorm" in parameters.keys():
+        sigma_lnorm = parameters['sigma_lnorm'].value
+    elif "b_hans" in parameters.keys():
+        b_hans = parameters['b_hans'].value
+        distribution = "hansen"
+
+    pRT_object.calc_flux(temperatures,
+                        abundances,
+                        gravity,
+                        MMW,
+                        contribution = False,
+                        fsed = parameters['fsed'].value,
+                        Kzz = Kzz_use,
+                        sigma_lnorm = sigma_lnorm,
+                        b_hans = b_hans,
+                        dist = distribution)
+    # Getting the model into correct units (W/m2/micron)
+    wlen_model = nc.c/pRT_object.freq/1e-4
+    wlen = nc.c/pRT_object.freq
+    f_lambda = pRT_object.flux*nc.c/wlen**2.
+    # convert to flux per m^2 (from flux per cm^2) cancels with step below
+    #f_lambda = f_lambda * 1e4
+    # convert to flux per micron (from flux per cm) cancels with step above
+    #f_lambda = f_lambda * 1e-4
+    # convert from ergs to Joule
+    f_lambda = f_lambda * 1e-7
+    spectrum_model = surf_to_meas(f_lambda,
+                                  R_pl,
+                                  parameters['D_pl'].value)
     return wlen_model, spectrum_model
 
 def isothermal_eqchem_transmission(pRT_object, \
@@ -503,7 +808,7 @@ def isothermal_eqchem_transmission(pRT_object, \
 
     # Make the abundance profile
     COs = parameters['C/O'].value * np.ones_like(pressures)
-    FeHs = parameters['[Fe/H]'].value * np.ones_like(pressures)
+    FeHs = parameters['Fe/H'].value * np.ones_like(pressures)
 
     abundances_interp = pm.interpol_abundances(COs, \
                                                FeHs, \
@@ -982,7 +1287,7 @@ def get_abundances(pressures, temperatures, line_species, cloud_species, paramet
     for key, val in Pbases.items():
         p_clouds.append(val)
     p_clouds = np.array(p_clouds)
-
+    #print(len(p_clouds),pressures.shape,parameters['pressure_scaling'].value,parameters['pressure_width'].value)
     if AMR:
         press_use, small_index = fixed_length_amr(p_clouds,
                                                   pressures,
