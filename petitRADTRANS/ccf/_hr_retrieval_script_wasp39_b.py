Fix spectral model MMR calculation and loading

petitRADTRANS/ccf/_hr_retrieval_script_wasp39_b.py
@@ -7,23 +7,19 @@ Try:
 If for some reason the script crashes.
 """
 import argparse
-import copy
 import os.path
 import sys
 import time
 
-import numpy as np
+import matplotlib.pyplot as plt
 
 # from petitRADTRANS.ccf.high_resolution_retrieval_wasp39_b import *
 # from petitRADTRANS.ccf.high_resolution_retrieval_toi270_c import *
-#from petitRADTRANS.ccf.high_resolution_retrieval_HD_189733_b import *
+# from petitRADTRANS.ccf.high_resolution_retrieval_HD_189733_b import *
 from petitRADTRANS.ccf.high_resolution_retrieval_HD_189733_b2 import *
 # from petitRADTRANS.ccf._high_resolution_retrieval2 import *
-from petitRADTRANS.ccf.model_containers import RetrievalSpectralModel
-from petitRADTRANS.ccf.spectra_utils import load_snr_file
 from petitRADTRANS.retrieval import Retrieval
 from petitRADTRANS.retrieval.plotting import contour_corner
-import matplotlib.pyplot as plt
 
 
 # Arguments definition
@@ -981,7 +977,7 @@ def main_hd(sim_id=0):
         }
         retrieval_directory = ''
 
-    return 0
+    # return 0
 
     for key in retrieval_parameters:
         if key == 'prt_object':
@@ -1085,7 +1081,7 @@ def main_hd2(sim_id=0):
 
     module_dir = os.path.abspath(os.path.dirname(__file__))
 
-    star_name = 'WASP-82'
+    star_name = 'HD 189733'#'WASP-82'
     planet_name = star_name + ' b'
     planet = Planet.get(planet_name)
 
@@ -1093,12 +1089,12 @@ def main_hd2(sim_id=0):
     # planet.equilibrium_temperature = 1209
 
     # line_species_str = ['CO_main_iso', 'CO_36', 'CH4_main_iso', 'H2O_main_iso']
-    line_species_str = all_species#['CO_main_iso', 'CH4_main_iso', 'H2O_main_iso']
+    line_species_str = ['CO_main_iso', 'H2O_main_iso']
     # line_species_str = ['CO_36', 'CH4_main_iso', 'H2O_main_iso']
     # line_species_str = ['CO_main_iso', 'CO_36', 'H2O_main_iso']
     # line_species_str = ['CO_main_iso', 'CO_36', 'CH4_main_iso']
 
-    retrieval_name = f't{planet_name}{sim_id}_12CO_13CO_CH4_H2O_t_18-28_noCO_36'
+    retrieval_name = f't{planet_name}{sim_id}_12CO_13CO_CH4_H2O_t_18-28_noCO_36_nonoise'
 
     if not os.path.isdir(
             os.path.join(module_dir, '..', '__tmp', 'test_retrieval', 'bins_' + planet_name.lower().replace(' ', '_'))
@@ -1114,7 +1110,7 @@ def main_hd2(sim_id=0):
 
     mode = 'transit'
     n_live_points = 100
-    add_noise = True
+    add_noise = False
 
     retrieval_directories = os.path.abspath(os.path.join(module_dir, '..', '__tmp', 'test_retrieval'))
 
@@ -1146,13 +1142,18 @@ def main_hd2(sim_id=0):
         # 'M': 76.89
     }
 
-    wavelengths_borders = wavelengths_borders[band]
-    integration_times_ref = integration_times_ref[band]
+    wavelengths_borders = [1.0, 1.7]#[3.1, 3.11]#wavelengths_borders[band]
+    integration_times_ref = 260#20.83#integration_times_ref[band]
 
     snr_file = os.path.join(module_dir, 'andes', star_name.replace(' ', '_'), f"ANDES_snrs.npz")
+    snr_file = os.path.join(module_dir, 'carmenes', planet.name.lower().replace(' ', '_'),
+                                         f"CARMENES_model_snr.npz")
+    # snr_file = os.path.join(module_dir, 'metis', 'SimMETIS', star_name.replace(' ', '_'),
+    #                         f"{star_name.replace(' ', '_')}_SNR_L-band_calibrated.txt")
     telluric_transmittance = os.path.join(module_dir, 'andes', 'sky', 'transmission',
-                                          f"transmission_1500_2500.dat")
+                                          f"transmission.dat")
     airmass = os.path.join(module_dir, 'andes', star_name.replace(' ', '_'), 'airmass_optimal.txt')
+    #airmass = os.path.join(module_dir, 'metis', 'brogi_crires_test', 'air.npy')
     variable_throughput = os.path.join(module_dir, 'metis', 'brogi_crires_test', 'algn.npy')
 
     wavelengths_instrument = None
@@ -1185,7 +1186,7 @@ def main_hd2(sim_id=0):
             'retrieval_model': retrieval_model,
             'wavelengths_instrument': model.model_parameters['output_wavelengths'],
             'observed_spectra': reduced_mock_observations,
-            'observations_uncertainties': model.model_parameters['uncertainties'],
+            'observations_uncertainties': model.model_parameters['reduced_uncertainties'],
             'prt_object': prt_object,
             'parameters': copy.deepcopy(model.model_parameters)
         }
@@ -1209,8 +1210,7 @@ def main_hd2(sim_id=0):
             'parameters': {}
         }
         retrieval_directory = ''
-
-    return 0
+        model = None
 
     for key in retrieval_parameters:
         if key == 'prt_object':
@@ -1295,7 +1295,22 @@ def main_hd2(sim_id=0):
         true_values = {retrieval_name: []}
 
         for p in parameter_dict[retrieval_name]:
-            true_values[retrieval_name].append(np.mean(retrieval_parameters['parameters'][p]))
+            if p not in retrieval_parameters['parameters']:
+                if p not in retrieval_parameters['parameters']['imposed_mass_mixing_ratios']:
+                    if p not in model.mass_mixing_ratios:
+                        raise KeyError(f"'{p}' from parameter dict not found in retrieval parameters")
+                    else:
+                        true_values[retrieval_name].append(
+                            np.mean(np.log10(model.mass_mixing_ratios[p]))
+                        )
+                else:
+                    true_values[retrieval_name].append(
+                        np.mean(np.log10(retrieval_parameters['parameters']['imposed_mass_mixing_ratios'][p]))
+                    )
+            else:
+                true_values[retrieval_name].append(
+                    np.mean(retrieval_parameters['parameters'][p])
+                )
 
         fig = contour_corner(
             sample_dict, parameter_dict, os.path.join(retrieval_directory, f'corner_{retrieval_name}.png'),
@@ -1416,137 +1431,137 @@ def _main_hd2(sim_id=0):
                 )
 
 
-def main_hd3(planet, sim_id):
-    from mpi4py import MPI
-
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    n_proc = comm.Get_size()
-
-    planet = a
-
-    module_dir = os.path.abspath(os.path.dirname(__file__))
-
-    mode = 'transit'
-    n_live_points = 100
-
-    star_names = [
-        'HD 189733',
-        'WASP-82',
-        'HAT-P-46',
-        'TOI-1130',
-        'LHS 1140'
-    ]
-
-    planet_names = []
-    planets = []
-
-    for star_name in star_names:
-        planet_names.append(star_name + ' b')
-        planets.append(Planet.get(planet_names[-1]))
-
-        if planets[-1].name == 'HD 189733 b':  # Paul's setup for HD 189733
-            planets[-1].equilibrium_temperature = 1209
-
-    line_species_strs = [
-        ['CO_main_iso', 'CO_36', 'CH4_main_iso', 'H2O_main_iso'],
-        ['CO_main_iso', 'CH4_main_iso', 'H2O_main_iso'],
-        ['CO_36', 'CH4_main_iso', 'H2O_main_iso'],
-        ['CO_main_iso', 'CO_36', 'H2O_main_iso'],
-        ['CO_main_iso', 'CO_36', 'CH4_main_iso']
-    ]
-
-    retrieval_base_names = []
-
-    for planet in planets:
-        for line_species_str in line_species_strs:
-            retrieval_species_names = []
-
-            for species in line_species_str:
-                species.replace('main_iso', '')
-                retrieval_species_names.append(species)
-
-            retrieval_base_names.append(
-                f"{planet.name.lower().replace(' ', '_')}_"
-                f"t_1.8-2.5um_"
-                f"{'_'.join(retrieval_species_names)}_"
-            )
-
-    if not os.path.isdir(
-            os.path.join(module_dir, '..', '__tmp', 'test_retrieval', 'bins_' + planet_name.lower().replace(' ', '_'))
-    ):
-        os.mkdir(
-            os.path.join(module_dir, '..', '__tmp', 'test_retrieval', 'bins_' + planet_name.lower().replace(' ', '_'))
-        )
-
-    add_noise = False
-
-    retrieval_directories = os.path.abspath(os.path.join(module_dir, '..', '__tmp', 'test_retrieval'))
-
-    load_from = None
-    # load_from = os.path.join(retrieval_directories, f't0_kp_vr_CO_H2O_79-80_{mode}_200lp_np')
-    # load_from = os.path.join(retrieval_directories, f't{sim_id}_vttt_p_kp_vr_CO_H2O_79-80_{mode}_30lp')
-    # load_from = os.path.join(retrieval_directories, f't1_tt2_p_mr_kp_vr_CO_H2O_79-80_{mode}_100lp')
-
-    band = 'K'
-
-    wavelengths_borders = {
-        'K': [1.8, 2.8],
-        # 'K': [2.28, 2.42],
-        # 'K': [2.15, 2.4],
-        # 'M': [4.79, 4.80]  # [4.5, 5.5],
-    }
-
-    wrange_0 = wavelengths_borders[band] * np.array([1.001, 0.999])
-    Nwranges = 100
-    wranges = np.linspace(wrange_0[0], wrange_0[1], int(Nwranges + 1))
-    wavelengths_borders[band] = np.array([wranges[sim_id], wranges[sim_id + 1]])
-
-    if wavelengths_borders[band][-1] > 2.5:
-        print("End of transmission file")
-        return 0
-
-    integration_times_ref = {
-        'K': 60,
-        # 'M': 76.89
-    }
-
-    snr_file = os.path.join(module_dir, 'andes', star_name.replace(' ', '_'), f"ANDES_snrs.npz")
-    telluric_transmittance = os.path.join(module_dir, 'andes', 'sky', 'sky', 'transmission',
-                                          f"transmission_1500_2500.dat")
-    # airmass = os.path.join(module_dir, 'carmenes', 'hd_189733_b', 'air.npy')
-    airmass = os.path.join(module_dir, 'andes', star_name.replace(' ', '_'), 'airmass_optimal.txt')
-    variable_throughput = os.path.join(module_dir, 'metis', 'brogi_crires_test')
-
-    wavelengths_instrument = None
-    instrument_snr = None
-    plot = True
-    instrument_resolving_power = 1e5
-
-    for j, ls_str in enumerate(line_species_str):
-        if rank == j:
-            print(f"rank {j}, line list {ls_str}, wrange = {wavelengths_borders[band]}")
-            # Initialize parameters
-            retrieval_directory = os.path.abspath(
-                os.path.join(module_dir, '..', '__tmp', 'test_retrieval',
-                             'bins_' + planet_name.lower().replace(' ', '_'),
-                             retrieval_names[j])
-            )
-
-            retrieval_name, retrieval_directory, \
-                model, pressures, true_parameters, line_species, rayleigh_species, continuum_species, \
-                retrieval_model, \
-                wavelength_instrument, reduced_mock_observations, error \
-                = init_mock_observations(
-                    planet, ls_str, mode,
-                    retrieval_directory, retrieval_names[j], n_live_points,
-                    add_noise, band, wavelengths_borders, integration_times_ref,
-                    wavelengths_instrument=wavelengths_instrument, instrument_snr=instrument_snr, snr_file=snr_file,
-                    telluric_transmittance=telluric_transmittance, airmass=airmass,
-                    variable_throughput=variable_throughput,
-                    instrument_resolving_power=instrument_resolving_power,
-                    load_from=load_from, plot=plot
-                )
+# def main_hd3(planet, sim_id):
+#     from mpi4py import MPI
+#
+#     comm = MPI.COMM_WORLD
+#     rank = comm.Get_rank()
+#     n_proc = comm.Get_size()
+#
+#     planet = a
+#
+#     module_dir = os.path.abspath(os.path.dirname(__file__))
+#
+#     mode = 'transit'
+#     n_live_points = 100
+#
+#     star_names = [
+#         'HD 189733',
+#         'WASP-82',
+#         'HAT-P-46',
+#         'TOI-1130',
+#         'LHS 1140'
+#     ]
+#
+#     planet_names = []
+#     planets = []
+#
+#     for star_name in star_names:
+#         planet_names.append(star_name + ' b')
+#         planets.append(Planet.get(planet_names[-1]))
+#
+#         if planets[-1].name == 'HD 189733 b':  # Paul's setup for HD 189733
+#             planets[-1].equilibrium_temperature = 1209
+#
+#     line_species_strs = [
+#         ['CO_main_iso', 'CO_36', 'CH4_main_iso', 'H2O_main_iso'],
+#         ['CO_main_iso', 'CH4_main_iso', 'H2O_main_iso'],
+#         ['CO_36', 'CH4_main_iso', 'H2O_main_iso'],
+#         ['CO_main_iso', 'CO_36', 'H2O_main_iso'],
+#         ['CO_main_iso', 'CO_36', 'CH4_main_iso']
+#     ]
+#
+#     retrieval_base_names = []
+#
+#     for planet in planets:
+#         for line_species_str in line_species_strs:
+#             retrieval_species_names = []
+#
+#             for species in line_species_str:
+#                 species.replace('main_iso', '')
+#                 retrieval_species_names.append(species)
+#
+#             retrieval_base_names.append(
+#                 f"{planet.name.lower().replace(' ', '_')}_"
+#                 f"t_1.8-2.5um_"
+#                 f"{'_'.join(retrieval_species_names)}_"
+#             )
+#
+#     if not os.path.isdir(
+#             os.path.join(module_dir, '..', '__tmp', 'test_retrieval', 'bins_' + planet_name.lower().replace(' ', '_'))
+#     ):
+#         os.mkdir(
+#             os.path.join(module_dir, '..', '__tmp', 'test_retrieval', 'bins_' + planet_name.lower().replace(' ', '_'))
+#         )
+#
+#     add_noise = False
+#
+#     retrieval_directories = os.path.abspath(os.path.join(module_dir, '..', '__tmp', 'test_retrieval'))
+#
+#     load_from = None
+#     # load_from = os.path.join(retrieval_directories, f't0_kp_vr_CO_H2O_79-80_{mode}_200lp_np')
+#     # load_from = os.path.join(retrieval_directories, f't{sim_id}_vttt_p_kp_vr_CO_H2O_79-80_{mode}_30lp')
+#     # load_from = os.path.join(retrieval_directories, f't1_tt2_p_mr_kp_vr_CO_H2O_79-80_{mode}_100lp')
+#
+#     band = 'K'
+#
+#     wavelengths_borders = {
+#         'K': [1.8, 2.8],
+#         # 'K': [2.28, 2.42],
+#         # 'K': [2.15, 2.4],
+#         # 'M': [4.79, 4.80]  # [4.5, 5.5],
+#     }
+#
+#     wrange_0 = wavelengths_borders[band] * np.array([1.001, 0.999])
+#     Nwranges = 100
+#     wranges = np.linspace(wrange_0[0], wrange_0[1], int(Nwranges + 1))
+#     wavelengths_borders[band] = np.array([wranges[sim_id], wranges[sim_id + 1]])
+#
+#     if wavelengths_borders[band][-1] > 2.5:
+#         print("End of transmission file")
+#         return 0
+#
+#     integration_times_ref = {
+#         'K': 60,
+#         # 'M': 76.89
+#     }
+#
+#     snr_file = os.path.join(module_dir, 'andes', star_name.replace(' ', '_'), f"ANDES_snrs.npz")
+#     telluric_transmittance = os.path.join(module_dir, 'andes', 'sky', 'sky', 'transmission',
+#                                           f"transmission_1500_2500.dat")
+#     # airmass = os.path.join(module_dir, 'carmenes', 'hd_189733_b', 'air.npy')
+#     airmass = os.path.join(module_dir, 'andes', star_name.replace(' ', '_'), 'airmass_optimal.txt')
+#     variable_throughput = os.path.join(module_dir, 'metis', 'brogi_crires_test')
+#
+#     wavelengths_instrument = None
+#     instrument_snr = None
+#     plot = True
+#     instrument_resolving_power = 1e5
+#
+#     for j, ls_str in enumerate(line_species_str):
+#         if rank == j:
+#             print(f"rank {j}, line list {ls_str}, wrange = {wavelengths_borders[band]}")
+#             # Initialize parameters
+#             retrieval_directory = os.path.abspath(
+#                 os.path.join(module_dir, '..', '__tmp', 'test_retrieval',
+#                              'bins_' + planet_name.lower().replace(' ', '_'),
+#                              retrieval_names[j])
+#             )
+#
+#             retrieval_name, retrieval_directory, \
+#                 model, pressures, true_parameters, line_species, rayleigh_species, continuum_species, \
+#                 retrieval_model, \
+#                 wavelength_instrument, reduced_mock_observations, error \
+#                 = init_mock_observations(
+#                     planet, ls_str, mode,
+#                     retrieval_directory, retrieval_names[j], n_live_points,
+#                     add_noise, band, wavelengths_borders, integration_times_ref,
+#                     wavelengths_instrument=wavelengths_instrument, instrument_snr=instrument_snr, snr_file=snr_file,
+#                     telluric_transmittance=telluric_transmittance, airmass=airmass,
+#                     variable_throughput=variable_throughput,
+#                     instrument_resolving_power=instrument_resolving_power,
+#                     load_from=load_from, plot=plot
+#                 )
 
 
 if __name__ == '__main__':

petitRADTRANS/ccf/high_resolution_retrieval.py
@@ -11,7 +11,7 @@ import petitRADTRANS.nat_cst as nc
 from petitRADTRANS.ccf.ccf_utils import radiosity_erg_hz2radiosity_erg_cm
 from petitRADTRANS.ccf.mock_observation import add_telluric_lines, add_variable_throughput, \
     generate_mock_observations, get_mock_secondary_eclipse_spectra, get_mock_transit_spectra, get_orbital_phases
-from petitRADTRANS.ccf.model_containers import Planet, SpectralModel
+from petitRADTRANS.ccf.model_containers import Planet
 from petitRADTRANS.ccf.pipeline import simple_pipeline, pipeline_validity_test
 from petitRADTRANS.ccf.utils import calculate_reduced_chi2
 from petitRADTRANS.fort_rebin import fort_rebin as fr
@@ -54,7 +54,7 @@ def _init_model(planet, w_bords, line_species_str, p0=1e-2):
     mass_fractions = {
         'H2': 0.74,
         'He': 0.24,
-       # line_species_str: 1e-3
+        # line_species_str: 1e-3
     }
     for species in line_species_str:
         mass_fractions[species] = 1e-3
@@ -115,7 +115,7 @@ def _init_model_old(planet, w_bords, line_species_str, p0=1e-2):
     mass_fractions = {
         'H2': 0.74,
         'He': 0.24,
-       # line_species_str: 1e-3
+        # line_species_str: 1e-3
     }
     for species in line_species_str:
         mass_fractions[species] = 1e-3
@@ -344,7 +344,10 @@ def _get_transit_retrieval_model(prt_object, parameters, pt_plot_mode=None, AMR=
             wavelengths=parameters['wavelengths_instrument'].value,
             airmass=parameters['airmass'].value,
             uncertainties=parameters['data_uncertainties'].value,
-            apply_throughput_removal=False
+            polynomial_fit_degree=1,
+            tellurics_mask_threshold=0.2,
+            apply_throughput_removal=True,
+            apply_telluric_lines_removal=True
         )
     else:
         spectrum_model = spectrum_model0
@@ -502,7 +505,8 @@ def init_mock_observations(planet, line_species_str, mode,
     ))[0]
 
     if snr_file_data is not None and instrument_snr is None:
-        instrument_snr = np.ma.masked_invalid(snr_file_data[wh, 1] / snr_file_data[wh, 2])
+        n = np.ma.masked_less_equal(snr_file_data[wh, 2], 0)
+        instrument_snr = np.ma.masked_invalid(snr_file_data[wh, 1] / n)
     else:
         instrument_snr = instrument_snr[wh]
 
@@ -550,7 +554,7 @@ def init_mock_observations(planet, line_species_str, mode,
         airmass = np.interp(x, xp, airmass)
         telluric_transmittance = np.exp(
             np.transpose(np.transpose(
-                np.ones((np.size(orbital_phases), np.size(wavelengths_instrument)))
+                np.ones((np.size(orbital_phases), np.size(wavelengths_instrument[0])))
                 * np.log(telluric_transmittance)
             ) * airmass)
         )
@@ -693,11 +697,13 @@ def init_mock_observations(planet, line_species_str, mode,
             number=1
         )
 
-        true_parameters['data'] = Param(mock_observations)
         true_parameters['true_noise'] = Param(noise)
 
         uncertainties = np.ones(mock_observations.shape) / instrument_snr
         true_parameters['data_uncertainties'] = Param(copy.copy(uncertainties))
+
+        mock_observations = np.ma.masked_where(uncertainties.mask, mock_observations)
+        true_parameters['data'] = Param(mock_observations)
 
         # Generate deformation matrix
         deformation_matrix = _get_deformation_matrix(
@@ -821,6 +827,8 @@ def init_mock_observations(planet, line_species_str, mode,
         full=True,
         apply_throughput_removal=apply_throughput_removal,
         apply_telluric_lines_removal=apply_telluric_lines_removal,
+        polynomial_fit_degree=1,
+        tellurics_mask_threshold=0.2
     )
 
     uncertainties *= np.abs(reduction_matrix)
@@ -849,7 +857,8 @@ def init_mock_observations(planet, line_species_str, mode,
     ts = copy.copy(true_spectra)
     ts = np.ma.masked_where(mock_observations.mask, ts)
     fmt, mr0t, _ = simple_pipeline(
-        ts, airmass=airmass, wavelengths=wavelengths_instrument,
+        ts, airmass=airmass,
+        wavelengths=wavelengths_instrument,
         uncertainties=true_parameters['data_uncertainties'].value, full=True,
         apply_throughput_removal=apply_throughput_removal,
         apply_telluric_lines_removal=apply_telluric_lines_removal
@@ -862,17 +871,24 @@ def init_mock_observations(planet, line_species_str, mode,
                                      apply_throughput_removal=apply_throughput_removal,
                                      apply_telluric_lines_removal=apply_telluric_lines_removal,
                                      full=True)
-    fs, mr, _ = simple_pipeline(ts * true_parameters['deformation_matrix'].value + noise, airmass=airmass,
-                                wavelengths=wavelengths_instrument,
-                                uncertainties=true_parameters['data_uncertainties'].value, full=True,
-                                apply_throughput_removal=apply_throughput_removal,
-                                apply_telluric_lines_removal=apply_telluric_lines_removal,
-                                )
+    fs, mr, _ = simple_pipeline(
+        spectrum=ts * true_parameters['deformation_matrix'].value + noise,
+        uncertainties=true_parameters['data_uncertainties'].value,
+        wavelengths=wavelengths_instrument,
+        airmass=airmass,
+        full=True,
+        apply_throughput_removal=apply_throughput_removal,
+        apply_telluric_lines_removal=apply_telluric_lines_removal,
+        polynomial_fit_degree=1,
+        tellurics_mask_threshold=0.2
+    )
 
     # Check pipeline validity
+    print(np.max(np.abs(r - ts * mr0t)))
+    print(np.max(np.abs(reduced_mock_observations - (ts * true_parameters['deformation_matrix'].value + noise) * mr)))
     assert np.allclose(r, ts * mr0t, atol=1e-14, rtol=1e-14)
     assert np.allclose(reduced_mock_observations, (ts * true_parameters['deformation_matrix'].value + noise) * mr,
-                       atol=1e-14, rtol=1e-14)
+                       atol=1e-9, rtol=1e-9)
 
     print('Pipeline validity check OK')
 
@@ -1066,7 +1082,6 @@ def init_mock_observations(planet, line_species_str, mode,
             ),
             file_name=os.path.join(retrieval_directory, 'pipeline_validity.png')
         )
-
 
     save_all(
         directory=retrieval_directory,

petitRADTRANS/ccf/mock_observation.py
@@ -388,9 +388,10 @@ def generate_mock_observations(wavelength_model, planet_spectrum_model,
     if np.ndim(telluric_transmittance) == 1:
         telluric_transmittance = np.ones(data_shape) * telluric_transmittance
     elif np.ndim(telluric_transmittance) == 2:
-        telluric_transmittance = \
-            np.ones(np.append(planet_velocities.size, telluric_transmittance.shape)) * telluric_transmittance
-        telluric_transmittance = np.moveaxis(telluric_transmittance, 0, 1)
+        pass
+        # telluric_transmittance = \
+        #     np.ones(np.append(planet_velocities.size, telluric_transmittance.shape)) * telluric_transmittance
+        # telluric_transmittance = np.moveaxis(telluric_transmittance, 0, 1)
 
     if np.shape(telluric_transmittance)[-1] != np.shape(wavelength_instrument)[-1]:
         raise ValueError(f"Telluric transmittance (shape {np.shape(telluric_transmittance)}) "

petitRADTRANS/ccf/model_containers.py
@@ -3711,8 +3711,11 @@ class SpectralModel2(BaseSpectralModel):
 
                     for species in mass_mixing_ratios:
                         if species not in imposed_mass_mixing_ratios:
-                            mass_mixing_ratios[species][i] = \
-                                mass_mixing_ratios[species][i] * (1 - m_sum_imposed_species[i]) / m_sum_species[i]
+                            if m_sum_species[i] > 0:
+                                mass_mixing_ratios[species][i] = \
+                                    mass_mixing_ratios[species][i] * (1 - m_sum_imposed_species[i]) / m_sum_species[i]
+                            else:
+                                mass_mixing_ratios[species][i] = mass_mixing_ratios[species][i] / m_sum_total[i]
                 elif m_sum_total[i] == 0:
                     if verbose:
                         print(f"sum of species mass fraction ({m_sum_species[i]} + {m_sum_imposed_species[i]}) "
@@ -3744,6 +3747,12 @@ class SpectralModel2(BaseSpectralModel):
                         # Use He/H2 ratio in argument
                         mass_mixing_ratios['H2'][i] = (1 - m_sum_total[i]) / (1 + heh2_ratio)
                         mass_mixing_ratios['He'][i] = mass_mixing_ratios['H2'][i] * heh2_ratio
+                else:
+                    mass_mixing_ratios['H2'] = np.zeros(np.shape(pressures))
+                    mass_mixing_ratios['He'] = np.zeros(np.shape(pressures))
+        else:
+            mass_mixing_ratios['H2'] = np.zeros(np.shape(pressures))
+            mass_mixing_ratios['He'] = np.zeros(np.shape(pressures))
 
         return mass_mixing_ratios
 
@@ -3918,13 +3927,13 @@ class SpectralModel2(BaseSpectralModel):
         # imposed_mass_mixing_ratios = {}
         kwargs = {'imposed_mass_mixing_ratios': {}}
 
-        for species in radtrans.line_species:
-            # TODO mass mixing ratio dict initialization more general
-            spec = species.split('_R_')[0]  # deal with the naming scheme for binned down opacities
-
-            if spec in parameters:
-                # TODO move that to RetrievalSpectralModel
-                kwargs['imposed_mass_mixing_ratios'][species] = 10 ** parameters[spec] * np.ones_like(pressures)
+        # for species in radtrans.line_species:
+        #     # TODO mass mixing ratio dict initialization more general
+        #     spec = species.split('_R_')[0]  # deal with the naming scheme for binned down opacities
+        #
+        #     if spec in parameters:
+        #         # TODO move that to RetrievalSpectralModel
+        #         kwargs['imposed_mass_mixing_ratios'][species] = 10 ** parameters[spec] * np.ones_like(pressures)
 
         for parameter, value in parameters.items():
             # TODO move that to RetrievalSpectralModel
@@ -3932,11 +3941,9 @@ class SpectralModel2(BaseSpectralModel):
                 kwargs[parameter.split('log10_', 1)[-1]] = 10 ** value
             else:
                 if parameter == 'imposed_mass_mixing_ratios':
-                    if 'imposed_mass_mixing_ratios' in kwargs:
-                        if kwargs[parameter] is None:
-                            kwargs[parameter] = copy.copy(value)
-                    else:
-                        kwargs[parameter] = copy.copy(value)
+                    for species, mass_mixing_ratios in parameters[parameter].items():
+                        if species not in kwargs[parameter]:
+                            kwargs[parameter][species] = copy.copy(mass_mixing_ratios)
                 else:
                     kwargs[parameter] = copy.copy(value)
 

petitRADTRANS/ccf/pipeline.py
@@ -706,9 +706,10 @@ def simple_pipeline(spectrum, uncertainties=None,
         polynomial_fit_degree: degree of the polynomial fit of the Earth atmospheric transmittance
         apply_throughput_removal: if True, apply the throughput removal correction
         apply_telluric_lines_removal: if True, apply the telluric lines removal correction
+        full: if True, return the reduced matrix and reduced uncertainties in addition to the reduced spectrum
 
     Returns:
-        Reduced spectral data, reduction matrix and uncertainties after reduction
+        Reduced spectral data (and reduction matrix and uncertainties after reduction if full is True)
     """
     # Initialize reduction matrix
     reduction_matrix = np.ma.ones(spectrum.shape)

petitRADTRANS/ccf/utils.py
@@ -1,4 +1,3 @@
-import copy
 import os
 
 import h5py
@@ -6,6 +5,20 @@ import warnings
 import numpy as np
 
 module_dir = os.path.dirname(__file__)  # TODO find a cleaner way to do this?
+
+
+def bytes2str(obj):
+    if hasattr(obj, '__iter__') and not isinstance(obj, bytes):
+        new_obj = []
+
+        for o in obj:
+            new_obj.append(bytes2str(o))
+
+        return np.array(new_obj)
+    elif isinstance(obj, bytes):
+        return str(obj, 'utf-8')
+    else:
+        return obj
 
 
 def class_init_args2class_args(string):
@@ -54,10 +67,7 @@ def hdf52dict(hdf5_file):
 
     for key in hdf5_file:
         if isinstance(hdf5_file[key], h5py.Dataset):
-            if isinstance(hdf5_file[key][()], bytes):
-                dictionary[key] = str(hdf5_file[key][()], 'utf-8')
-            else:
-                dictionary[key] = hdf5_file[key][()]
+            dictionary[key] = bytes2str(hdf5_file[key][()])
         elif isinstance(hdf5_file[key], h5py.Group):
             dictionary[key] = hdf52dict(hdf5_file[key])
         else: